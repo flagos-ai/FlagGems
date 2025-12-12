f2741a9861faa29c972118ecb2656d3858eb8a6c
fanxiangyu <fxyfxy777@163.com>
2025-12-12 10:07:22 +0800

    rm unuse test

--- benchmark/test_paddle_tensor_constructor_perf.py
+++ benchmark/test_paddle_tensor_constructor_perf.py
@@ -47,20 +47,3 @@ def test_tensor_constructor_benchmark(op_name, torch_op, input_fn):
     bench = GenericBenchmark(input_fn=input_fn, op_name=op_name, torch_op=torch_op)
     bench.run()
 
-
-@pytest.mark.skipif(
-    vendor_name == "kunlunxin" or vendor_name == "hygon", reason="RESULT TODOFIX"
-)
-@pytest.mark.skipif(vendor_name == "mthreads", reason="RuntimeError")
-@pytest.mark.randperm
-def test_perf_randperm():
-    def randperm_input_fn(shape, dtype, device):
-        yield {"n": shape[0], "dtype": dtype, "device": device},
-
-    bench = GenericBenchmark(
-        input_fn=randperm_input_fn,
-        op_name="randperm",
-        torch_op=torch.randperm,
-        dtypes=[torch.int32, torch.int64],
-    )
-    bench.run()
