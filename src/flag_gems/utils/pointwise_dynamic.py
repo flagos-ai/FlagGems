9dee7df35422b7dd1e9180d9f3f743963b185e55
0x45f <wangzhen@baai.ac.cn>
2025-12-19 09:43:45 +0000

    Fix

--- src/flag_gems/utils/pointwise_dynamic.py
+++ src/flag_gems/utils/pointwise_dynamic.py
@@ -379,11 +379,11 @@ class KernelGenerator:
                     code.writeline(f"{stride_args}, # strides for out{i}")
 
                 # task space, used to reconstruct multi index
-                task_space_args = _cs(f"s{i}: int" for i in range(ndim))
+                task_space_args = _cs(f"s{i}: tl.int64" for i in range(ndim))
                 code.writeline(f"{task_space_args}, # task_space")
 
                 # number of tasks, used to compute mask
-                code.writeline("num_tasks: int,")
+                code.writeline("num_tasks: tl.int64,")
 
             # tile size & tiles_per_cta, gsl style
             if ndim > 0:
