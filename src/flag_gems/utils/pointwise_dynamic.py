Fix numel overflow int32 for pointwise dynamic

src/flag_gems/utils/pointwise_dynamic.py
@@ -320,11 +320,11 @@ class KernelGenerator:
                         )
 
                 # task space, used to reconstruct multi index
-                task_space_args = _cs(f"s{i}: int" for i in range(ndim))
+                task_space_args = _cs(f"s{i}: tl.int64" for i in range(ndim))
                 code.writeline(f"{task_space_args}, # task_space")
 
                 # number of tasks, used to compute mask
-                code.writeline("num_tasks: int,")
+                code.writeline("num_tasks: tl.int64,")
 
             # tile size & tiles_per_cta, gsl style
             if ndim > 0:
@@ -1135,11 +1135,9 @@ class PointwiseDynamicFunction:
 
         tensors = out_tensors + in_tensors
         INT32_MAX = torch.iinfo(torch.int32).max
-        use_fast_path = self.use_fast_path(tensors)
-        fast_path_allowed = (
-            use_fast_path and tensors and tensors[0].numel() <= INT32_MAX
-        )
-        if fast_path_allowed:  # dimension collapse & use physical ordering
+        if tensors[0].numel() > INT32_MAX:
+            self.config.prefer_block_pointer = False
+        if self.use_fast_path(tensors):  # dimension collapse & use physical ordering
             # if self.use_fast_path(tensors):  # dimension collapse & use physical ordering
             allocated_outputs = [
                 torch.empty_like(tensors[0], dtype=dtype)