f51e16f774285e77c358de0ea45bacba0e8cba8c
YoihenBachu <thdamyoihenba@gmail.com>
2026-01-12 22:13:36 +0530

    Fixed pre-commit issue

--- src/flag_gems/ops/cosh.py
+++ src/flag_gems/ops/cosh.py
@@ -1,17 +1,18 @@
 import logging
+
 import triton
 import triton.language as tl
 
 from flag_gems.utils import pointwise_dynamic, tl_extra_shim
 
-_cosh = tl_extra_shim.cosh  # Import the shim function
+_cosh = tl_extra_shim.cosh
 logger = logging.getLogger(__name__)
 
 
 @pointwise_dynamic(promotion_methods=[(0, "INT_TO_FLOAT")])
 @triton.jit
 def cosh_func(x):
-    return _cosh(x.to(tl.float32))  # Use it just like tl.cosh()
+    return _cosh(x.to(tl.float32))
 
 
 def cosh(A):
