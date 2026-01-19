97e64c31f5934ae0d64d89a9e8f7cab14bc373ed
lihaiqiang <lihaiqiang@tsingmicro.com>
2026-01-19 16:31:03 +0800

    add TSINGMICRO txda backend

--- src/flag_gems/runtime/backend/_tsingmicro/__init__.py
+++ src/flag_gems/runtime/backend/_tsingmicro/__init__.py
@@ -1,9 +1,15 @@
 from backend_utils import VendorInfoBase  # noqa: E402
 
 vendor_info = VendorInfoBase(
+<<<<<<< HEAD
     vendor_name="tsingmicro", 
     device_name="txda", 
     device_query_cmd="tsm_smi", 
+=======
+    vendor_name="tsingmicro",
+    device_name="txda",
+    device_query_cmd="tsm_smi",
+>>>>>>> 055fd0b6 (add TSINGMICRO txda backend)
     dispatch_key="PrivateUse1",
 )
 
