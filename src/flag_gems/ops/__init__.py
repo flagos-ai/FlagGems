c16d8a78a70a958490d9c26ee88c0c77ecdc90c6
Li Guancheng <153363659+LGCCCCCCC@users.noreply.github.com>
2026-01-24 20:47:37 +0800

    Modified __init__.py for easier testing
    I commented out the parts of the code that import max_pool3d and avg_pool3d.
    
    Signed-off-by: Li Guancheng <153363659+LGCCCCCCC@users.noreply.github.com>

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -23,7 +23,7 @@ from flag_gems.ops.attention import (
     scaled_dot_product_attention_forward,
 )
 from flag_gems.ops.avg_pool2d import avg_pool2d, avg_pool2d_backward
-from flag_gems.ops.avg_pool3d import avg_pool3d, avg_pool3d_backward
+# from flag_gems.ops.avg_pool3d import avg_pool3d, avg_pool3d_backward
 from flag_gems.ops.baddbmm import baddbmm
 from flag_gems.ops.batch_norm import batch_norm, batch_norm_backward
 from flag_gems.ops.bitwise_and import (
@@ -133,11 +133,11 @@ from flag_gems.ops.max_pool2d_with_indices import (
     max_pool2d_backward,
     max_pool2d_with_indices,
 )
-from flag_gems.ops.max_pool3d import (
-    max_pool3d,
-    max_pool3d_backward,
-    max_pool3d_with_indices,
-)
+# from flag_gems.ops.max_pool3d import (
+#     max_pool3d,
+#     max_pool3d_backward,
+#     max_pool3d_with_indices,
+# )
 from flag_gems.ops.maximum import maximum
 from flag_gems.ops.mean import mean, mean_dim
 from flag_gems.ops.min import min, min_dim
@@ -274,8 +274,8 @@ __all__ = [
     "argmin",
     "avg_pool2d",
     "avg_pool2d_backward",
-    "avg_pool3d",
-    "avg_pool3d_backward",
+    # "avg_pool3d",
+    # "avg_pool3d_backward",
     "atan",
     "atan_",
     "baddbmm",
@@ -413,9 +413,9 @@ __all__ = [
     "maximum",
     "max_pool2d_with_indices",
     "max_pool2d_backward",
-    "max_pool3d",
-    "max_pool3d_backward",
-    "max_pool3d_with_indices",
+    # "max_pool3d",
+    # "max_pool3d_backward",
+    # "max_pool3d_with_indices",
     "mean",
     "mean_dim",
     "min",
