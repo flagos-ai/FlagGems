ef4c0210c38d7d11aa178eac9cee25ac5c2749e1
kiddyjinjin <kiddyjinjin@gmail.com>
2026-01-27 05:08:47 +0000

    update device info

--- src/flag_gems/ops/get_scheduler_metadata.py
+++ src/flag_gems/ops/get_scheduler_metadata.py
@@ -6,6 +6,8 @@ import torch
 import triton
 import triton.language as tl
 
+from flag_gems.utils.device_info import get_device_capability
+
 logger = logging.getLogger(__name__)
 
 
@@ -155,7 +157,7 @@ def get_optimal_block_mn(
     varlen_and_split=False,
     append_kv=False,
 ):
-    arch_cap = torch.cuda.get_device_capability(device)
+    arch_cap = get_device_capability()
     arch = arch_cap[0] * 10 + arch_cap[1]
 
     if arch >= 90:
@@ -627,9 +629,11 @@ def get_scheduler_metadata(
         effective_window_left >= 0 or effective_window_right >= 0
     ) and not final_is_causal
 
-    arch_cap = torch.cuda.get_device_capability(device)
+    arch_cap = get_device_capability()
     arch = arch_cap[0] * 10 + arch_cap[1]
-    num_sm = torch.cuda.get_device_properties(device).multi_processor_count - sm_margin
+    from flag_gems.utils.device_info import get_device_info
+
+    num_sm = get_device_info().sm_count - sm_margin
 
     softcap = 1.0 if has_softcap else 0.0
 
