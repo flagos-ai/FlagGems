68ddccdb488e5bbde828f1e6896de561f7a31f05
kiddyjinjin <kiddyjinjin@gmail.com>
2026-02-10 02:19:56 +0000

    update quantile

--- benchmark/test_special_perf.py
+++ benchmark/test_special_perf.py
@@ -108,62 +108,6 @@ def test_perf_grouped_topk():
     bench.run()
 
 
-def native_per_token_group_quant_fp8(
-    x, group_size, eps=1e-10, dtype=None, scale_ue8m0=False
-):
-    if dtype is None:
-        dtype = flag_gems.SUPPORTED_FP8_DTYPE
-
-    assert (
-        x.shape[-1] % group_size == 0
-    ), "the last dimension of `x` cannot be divisible by `group_size`"
-    assert x.is_contiguous(), "`x` is not contiguous"
-
-    finfo = torch.finfo(dtype)
-    fp8_min = finfo.min
-    fp8_max = finfo.max
-
-    x_ = x.reshape(x.numel() // group_size, group_size)
-    amax = x_.abs().max(dim=-1, keepdim=True)[0].clamp(min=eps).to(torch.float32)
-    x_s = amax / fp8_max
-    if scale_ue8m0:
-        min_val = torch.tensor(1e-10, dtype=x_s.dtype, device=x_s.device)
-        x_s = torch.exp2(torch.ceil(torch.log2(torch.maximum(x_s.abs(), min_val))))
-    x_q = (x_ / x_s).clamp(min=fp8_min, max=fp8_max).to(dtype)
-    x_q = x_q.reshape(x.shape)
-    x_s = x_s.reshape(x.shape[:-1] + (x.shape[-1] // group_size,))
-
-    return x_q, x_s
-
-
-class PerTokenGroupQuantFp8Benchmark(Benchmark):
-    """
-    benchmark for per_token_group_quant_fp8
-    """
-
-    def get_input_iter(self, cur_dtype):
-        for config in self.shapes:
-            yield from self.per_token_group_quant_fp8_input_fn(
-                config, cur_dtype, self.device
-            )
-
-    def per_token_group_quant_fp8_input_fn(self, config, dtype, device):
-        num_tokens, d, group_size, scale_ue8m0 = config
-        x = torch.rand(num_tokens, d, dtype=dtype, device=device)
-        yield x, group_size, 1e-10, None, {"scale_ue8m0": scale_ue8m0}
-
-
-@pytest.mark.per_token_group_quant_fp8
-def test_perf_per_token_group_quant_fp8():
-    bench = PerTokenGroupQuantFp8Benchmark(
-        op_name="per_token_group_quant_fp8",
-        torch_op=native_per_token_group_quant_fp8,
-        dtypes=[torch.bfloat16],
-    )
-    bench.set_gems(flag_gems.per_token_group_quant_fp8)
-    bench.run()
-
-
 @pytest.mark.skipif(
     SkipVersion("vllm", "<0.9"),
     reason="The version prior to 0.9 does not include the grouped_topk kernel.",
