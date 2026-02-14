f2191e94983c998970c384c48bc7afdc4213c035
LuyuZhang00 <barry_zly@163.com>
2026-02-14 07:02:29 +0000

    add log10 operator

---
+++ src/flag_gems/ops/log10.py
@@ -0,0 +1,32 @@
+import logging
+
+import triton
+import triton.language as tl
+
+from flag_gems.utils import pointwise_dynamic
+
+logger = logging.getLogger(__name__)
+
+@pointwise_dynamic(promotion_methods=[(0, "COMPLEX_TO_FLOAT")])
+@triton.jit
+def log10_func(x):
+    # log10(x) = log2(x) / log2(10) = log2(x) * log10(2)
+    log10_2: tl.constexpr = 0.3010299956639812
+    return tl.log2(x.to(tl.float32)) * log10_2
+
+
+def log10(A):
+    logger.debug("GEMS LOG10")
+    return log10_func(A)
+
+
+def log10_(A):
+    logger.debug("GEMS LOG10_")
+    log10_func(A, out0=A)
+    return A
+
+
+def log10_out(A, out):
+    logger.debug("GEMS LOG10_OUT")
+    log10_func(A, out0=out)
+    return out
