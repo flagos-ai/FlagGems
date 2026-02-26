b3d19a2e0a812c93127bb4473e18506e2fef3260
chongzhou.yang <chongzhou.yang@enflame-tech.com>
2026-02-26 11:11:03 +0800

    remove print in argmax

--- src/flag_gems/runtime/backend/_enflame/ops/gcu300/argmax.py
+++ src/flag_gems/runtime/backend/_enflame/ops/gcu300/argmax.py
@@ -143,7 +143,6 @@ def argmax(inp, dim=None, keepdim=False, *, dtype=None):
                 out_shape[dim] = 1
             else:
                 del out_shape[dim]
-            print("inp.numel() == 0")
             return torch.zeros(out_shape, dtype=torch.int32, device=inp.device).to(torch.int64)
         N = shape[dim]
         M = math.prod(shape[:dim])
