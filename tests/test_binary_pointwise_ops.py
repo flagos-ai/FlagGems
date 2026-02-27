ac67d098eccc1c296917b53a92ec98405bbce2b4
taooo <gumptao2997@gmail.com>
2026-02-27 14:14:54 +0800

    Add greater operator implementation, tests and benchmark

--- tests/test_binary_pointwise_ops.py
+++ tests/test_binary_pointwise_ops.py
@@ -2166,3 +2166,34 @@ def test_accuracy_addcdiv(shape, dtype):
         res_out = torch.addcdiv(res_inp, t1, t2, value=v)
 
     gems_assert_close(res_out, ref_out, dtype)
+
+
+@pytest.mark.greater
+@pytest.mark.parametrize("shape", POINTWISE_SHAPES)
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+def test_accuracy_greater(shape, dtype):
+    inp1 = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    inp2 = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    ref_inp1 = to_reference(inp1)
+    ref_inp2 = to_reference(inp2)
+
+    ref_out = torch.greater(ref_inp1, ref_inp2)
+    with flag_gems.use_gems():
+        res_out = torch.greater(inp1, inp2)
+
+    gems_assert_equal(res_out, ref_out)
+
+
+@pytest.mark.greater
+@pytest.mark.parametrize("shape", POINTWISE_SHAPES)
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+def test_accuracy_greater_scalar(shape, dtype):
+    inp1 = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    ref_inp1 = to_reference(inp1)
+    inp2 = 0
+
+    ref_out = torch.greater(ref_inp1, inp2)
+    with flag_gems.use_gems():
+        res_out = torch.greater(inp1, inp2)
+
+    gems_assert_equal(res_out, ref_out)
