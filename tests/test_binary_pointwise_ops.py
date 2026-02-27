c19009b9cf9b33f244151030edd66941f043f90c
taooo <gumptao2997@gmail.com>
2026-02-27 14:12:52 +0800

    Add clip operator implementation, tests and benchmark

--- tests/test_binary_pointwise_ops.py
+++ tests/test_binary_pointwise_ops.py
@@ -500,6 +500,49 @@ def test_accuracy_clamp_min_(shape, dtype):
     gems_assert_equal(res_out, ref_out)
 
 
+@pytest.mark.clip
+@pytest.mark.parametrize("shape", POINTWISE_SHAPES)
+@pytest.mark.parametrize("maxi", SCALARS)
+@pytest.mark.parametrize("mini", SCALARS)
+@pytest.mark.parametrize("isnone", [None, "max", "min"])
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+def test_accuracy_clip(shape, maxi, mini, isnone, dtype):
+    inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    if isnone == "min":
+        mini = None
+    elif isnone == "max":
+        maxi = None
+    ref_inp = to_reference(inp)
+
+    ref_out = torch.clip(ref_inp, min=mini, max=maxi)
+    with flag_gems.use_gems():
+        res_out = torch.clip(inp, min=mini, max=maxi)
+
+    gems_assert_equal(res_out, ref_out)
+
+
+@pytest.mark.inplace
+@pytest.mark.clip_
+@pytest.mark.parametrize("shape", POINTWISE_SHAPES)
+@pytest.mark.parametrize("maxi", SCALARS)
+@pytest.mark.parametrize("mini", SCALARS)
+@pytest.mark.parametrize("isnone", [None, "max", "min"])
+@pytest.mark.parametrize("dtype", FLOAT_DTYPES)
+def test_accuracy_clip_(shape, maxi, mini, isnone, dtype):
+    inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    if isnone == "min":
+        mini = None
+    elif isnone == "max":
+        maxi = None
+    ref_inp = to_reference(inp.clone())
+
+    ref_out = torch.clip_(ref_inp, min=mini, max=maxi)
+    with flag_gems.use_gems():
+        res_out = torch.clip_(inp, min=mini, max=maxi)
+
+    gems_assert_equal(res_out, ref_out)
+
+
 @pytest.mark.div
 @pytest.mark.parametrize("shape", POINTWISE_SHAPES)
 @pytest.mark.parametrize("dtype", FLOAT_DTYPES)
