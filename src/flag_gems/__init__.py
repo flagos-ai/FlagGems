0a2d57d4eaa86630ac6fa9106f4bfac6c114f6e8
taooo <gumptao2997@gmail.com>
2026-02-27 14:10:46 +0800

    Add _is_all_true operator implementation, tests and benchmark

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -29,6 +29,7 @@ def torch_ge(v):
 
 _FULL_CONFIG = (
     ("_flash_attention_forward", flash_attention_forward),
+    ("_is_all_true", _is_all_true),
     ("_log_softmax", log_softmax),
     ("_log_softmax_backward_data", log_softmax_backward),
     ("_softmax", softmax),
