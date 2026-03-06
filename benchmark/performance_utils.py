ae35905dc90c7a4145f95e17a03e57d772e9b4c1
baoqi.liu <baoqi.liu@enflame-tech.com>
2026-03-06 09:49:45 +0800

    remove redundant check for allow_tf32

--- benchmark/performance_utils.py
+++ benchmark/performance_utils.py
@@ -36,8 +36,6 @@ if device == "musa":
 elif device == "npu":
     torch.backends.cuda.matmul.allow_tf32 = False
     torch.backends.cudnn.allow_tf32 = False
-elif device != "gcu":
-    torch_backend_device.matmul.allow_tf32 = False
 else:
     torch_backend_device.matmul.allow_tf32 = False
 
