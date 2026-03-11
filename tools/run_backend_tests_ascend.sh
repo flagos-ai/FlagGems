9d09253309c89e6b8bdad39eaa8a2e3b2afa1fea
liuxh <liuxh0217@163.com>
2026-03-11 21:59:27 +0800

    clarify the set_env.sh

--- tools/run_backend_tests_ascend.sh
+++ tools/run_backend_tests_ascend.sh
@@ -4,7 +4,11 @@ VENDOR=${1:?"Usage: bash tools/run_backend_tests_ascend.sh <vendor>"}
 export GEMS_VENDOR=$VENDOR
 export TRITON_ALL_BLOCKS_PARALLEL=1
 
-source /usr/local/Ascend/ascend-toolkit/set_env.sh
+# Initialize Ascend environment variables.
+# This script is provided by the Huawei Ascend CANN toolkit installation.
+if [ -f /usr/local/Ascend/ascend-toolkit/set_env.sh ]; then
+    source /usr/local/Ascend/ascend-toolkit/set_env.sh
+fi
 
 source tools/run_command.sh
 
