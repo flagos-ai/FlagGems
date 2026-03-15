3b6637f3e7d649db4b2ec32501391ee582e18c03
lihaiqiang <lihaiqiang@dev.com>
2026-03-15 16:20:41 +0800

    [TSINGMICRO] add randn and randn_like for tsingmicro
    Change-Id: I7093c0ebdbeeebe0c6cec48f45cab50b1f044f8a

--- src/flag_gems/runtime/backend/_tsingmicro/ops/__init__.py
+++ src/flag_gems/runtime/backend/_tsingmicro/ops/__init__.py
@@ -6,10 +6,13 @@ from .isin import isin
 from .kron import kron
 from .masked_select import masked_select
 from .mm import mm, mm_out
+from .randn import randn
+from .randn_like import randn_like
 from .rms_norm import rms_norm
 from .stack import stack
 from .unique import _unique2
 from .zeros import zeros, zero_
+
 
 __all__ = [
     "argmax",
@@ -19,6 +22,8 @@ __all__ = [
     "masked_select",
     "mm",
     "mm_out",
+    "randn",
+    "randn_like",
     "rms_norm",
     "stack",
     "kron",
