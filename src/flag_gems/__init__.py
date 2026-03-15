1d2500ed15b837d5cc735c209ead425408578fae
joehudson3 <joehudson3@users.noreply.github.com>
2026-03-15 08:50:48 -0600

    style: fix import ordering (isort)

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -8,7 +8,8 @@ from flag_gems import runtime
 from flag_gems.config import aten_patch_list, resolve_user_setting
 from flag_gems.experimental_ops import *  # noqa: F403
 from flag_gems.fused import *  # noqa: F403
-from flag_gems.logging_utils import setup_flaggems_logging, teardown_flaggems_logging
+from flag_gems.logging_utils import (setup_flaggems_logging,
+                                     teardown_flaggems_logging)
 from flag_gems.modules import *  # noqa: F403
 from flag_gems.ops import *  # noqa: F403
 from flag_gems.patches import *  # noqa: F403
