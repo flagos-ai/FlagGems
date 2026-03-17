4cb83d5ab2217ae3e1627363e533154675b2aa7b
Yanguan <yanguan_02@126.com>
2026-03-17 18:31:29 +0800

    Refactor: Remove input_guard decorator from utils.py to streamline tensor handling

--- src/flag_gems/runtime/backend/_ascend/fla/utils.py
+++ src/flag_gems/runtime/backend/_ascend/fla/utils.py
@@ -7,10 +7,6 @@
 # the following copyright notice:
 # Copyright (c) 2023-2025, Songlin Yang, Yu Zhang
 # ruff: noqa: E501
-import contextlib
-import functools
-from typing import Callable
-
 import torch
 import triton
 import triton.language as tl
@@ -31,40 +27,6 @@ def prepare_chunk_offsets(cu_seqlens: torch.LongTensor, chunk_size: int) -> torc
     return torch.cat(
         [cu_seqlens.new_tensor([0]), triton.cdiv(prepare_lens(cu_seqlens), chunk_size)]
     ).cumsum(-1)
-
-
-def input_guard(fn: Callable[..., torch.Tensor]) -> Callable[..., torch.Tensor]:
-    """
-    A decorator to make sure all input tensors are contiguous and set the device based on input tensors.
-    """
-
-    @functools.wraps(fn)
-    def wrapper(*args, **kwargs):
-        contiguous_args = (i if not isinstance(i, torch.Tensor) else i.contiguous() for i in args)
-        contiguous_kwargs = {
-            k: (v if not isinstance(v, torch.Tensor) else v.contiguous()) for k, v in kwargs.items()
-        }
-
-        tensor = None
-        for arg in args:
-            if isinstance(arg, torch.Tensor):
-                tensor = arg
-                break
-        if tensor is None:
-            for value in kwargs.values():
-                if isinstance(value, torch.Tensor):
-                    tensor = value
-                    break
-
-        if tensor is not None:
-            ctx = torch.npu.device(tensor.device.index)
-        else:
-            ctx = contextlib.nullcontext()
-
-        with ctx:
-            return fn(*contiguous_args, **contiguous_kwargs)
-
-    return wrapper
 
 
 @triton.jit
