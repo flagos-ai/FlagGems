9f83d5fa8da2cd43572e7305281f66323f5d0943
factnn <1050552884@qq.com>
2026-03-19 22:02:47 +0800

    fix: rename kernel from floor__kernel to floor_kernel

--- src/flag_gems/ops/floor_.py
+++ src/flag_gems/ops/floor_.py
@@ -7,7 +7,7 @@ from flag_gems.runtime import torch_device_fn
 
 
 @triton.jit
-def floor__kernel(
+def floor_kernel(
     x_ptr,
     n_elements,
     BLOCK_SIZE: tl.constexpr,
@@ -60,7 +60,7 @@ def floor_(input):
     grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
 
     with torch_device_fn.device(x.device):
-        floor__kernel[grid](
+        floor_kernel[grid](
             x,
             n_elements,
             BLOCK_SIZE=BLOCK_SIZE,
