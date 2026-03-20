d36cea110c65de3ceabff74abc5c41207d6441fa
0x45f <wangzhen@baai.ac.cn>
2026-03-20 06:15:02 +0000

    Refine code

--- src/flag_gems/runtime/backend/_mthreads/ops/log_softmax.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/log_softmax.py
@@ -321,7 +321,7 @@ def log_softmax_backward_kernel_inner(
 
 
 def log_softmax(self, dim, half_to_float=False):
-    logger.debug("MTHREADS GEMS LOG_SOFTMAX")
+    logger.debug("GEMS_MTHREADS LOG_SOFTMAX")
 
     assert dim >= -self.ndim and dim < self.ndim, "Invalid dim"
     dim = dim % self.ndim
@@ -359,7 +359,7 @@ def log_softmax(self, dim, half_to_float=False):
 
 
 def log_softmax_backward(grad_output, output, dim, input_dtype):
-    logger.debug("MTHREADS GEMS LOG_SOFTMAX VJP")
+    logger.debug("GEMS_MTHREADS LOG_SOFTMAX BACKWARD")
 
     assert dim >= -output.ndim and dim < output.ndim, "Invalid dim"
     dim = dim % output.ndim
