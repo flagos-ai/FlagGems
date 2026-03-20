3a36547890de25b5dcbf87ee02e027378502ea76
factnn <1050552884@qq.com>
2026-03-20 13:49:52 +0800

    fix: rename kernel to sinh_kernel_ per naming convention

--- src/flag_gems/ops/sinh_.py
+++ src/flag_gems/ops/sinh_.py
@@ -7,7 +7,7 @@ from flag_gems.runtime import torch_device_fn
 
 
 @triton.jit
-def sinh__kernel_(x_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
+def sinh_kernel_(x_ptr, n_elements, BLOCK_SIZE: tl.constexpr):
     pid = tl.program_id(axis=0)
     block_start = pid * BLOCK_SIZE
     offsets = block_start + tl.arange(0, BLOCK_SIZE)
@@ -52,5 +52,5 @@ def sinh_(*args, **kwargs):
     grid = lambda meta: (triton.cdiv(n_elements, meta["BLOCK_SIZE"]),)
 
     with torch_device_fn.device(x.device):
-        sinh__kernel_[grid](x, n_elements, BLOCK_SIZE=BLOCK_SIZE)
+        sinh_kernel_[grid](x, n_elements, BLOCK_SIZE=BLOCK_SIZE)
     return x
