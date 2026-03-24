610ef45d0d4e9f54483ab33969f102134614b88a
factnn <1050552884@qq.com>
2026-03-24 19:53:38 +0800

    fix: codestyle fixes from pre-commit

--- benchmark/test_unary_pointwise_perf.py
+++ benchmark/test_unary_pointwise_perf.py
@@ -150,7 +150,7 @@ forward_inplace_operations = [
     ("atan_", torch.atan_, FLOAT_DTYPES),
     ("i0_", torch.Tensor.i0_, FLOAT_DTYPES),
     ("digamma_", lambda a: a.digamma_(), FLOAT_DTYPES),
-("arctanh_", lambda a: a.arctanh_(), FLOAT_DTYPES),
+    ("arctanh_", lambda a: a.arctanh_(), FLOAT_DTYPES),
     ("digamma_", lambda a: a.digamma_(), FLOAT_DTYPES),
     ("asinh_", lambda a: a.asinh_(), FLOAT_DTYPES),
     ("digamma_", lambda a: a.digamma_(), FLOAT_DTYPES),
