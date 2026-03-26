dbad0ac507cf247394dc1e47f79ea9327d9adfc3
huangyiqun <huangyiqun@mail.bnu.edu.cn>
2026-03-26 03:08:37 +0000

    update mul

--- src/flag_gems/ops/mul.py
+++ src/flag_gems/ops/mul.py
@@ -51,13 +51,12 @@ def mul(A, B):
     if A_is_complex or B_is_complex:
         # 1) A、B both are complex
         if A_is_complex and B_is_complex:
-            Ar = torch.view_as_real(A)
-            Br = torch.view_as_real(B)
-            ar, ai = Ar[..., 0], Ar[..., 1]
-            br, bi = Br[..., 0], Br[..., 1]
-            common_dtype = torch.promote_types(ar.dtype, br.dtype)
-            ar, ai = ar.to(common_dtype), ai.to(common_dtype)
-            br, bi = br.to(common_dtype), bi.to(common_dtype)
+            ar, ai = A.real, A.imag
+            br, bi = B.real, B.imag
+            common_dtype = torch.promote_types(
+                torch.promote_types(ar.dtype, ai.dtype),
+                torch.promote_types(br.dtype, bi.dtype),
+            )
 
             out_real = torch.empty((*ar.shape, 2), dtype=common_dtype, device=ar.device)
             mul_complex_kernel(
@@ -73,21 +72,25 @@ def mul(A, B):
             return out.to(torch.result_type(A, B))
         # 2) A complex, B real
         elif A_is_complex and not B_is_complex:
-            Ar = torch.view_as_real(A)
-            Br = B.unsqueeze(-1) if isinstance(B, torch.Tensor) else B
-            if isinstance(Br, torch.Tensor):
-                out_real = mul_func(Ar, Br)
+            ar, ai = A.real, A.imag
+            out_real = torch.empty((*ar.shape, 2), dtype=ar.dtype, device=ar.device)
+            if isinstance(B, torch.Tensor):
+                mul_func(ar, B, out0=out_real[..., 0])
+                mul_func(ai, B, out0=out_real[..., 1])
             else:
-                out_real = mul_func_scalar(Ar, Br)
+                mul_func_scalar(ar, B, out0=out_real[..., 0])
+                mul_func_scalar(ai, B, out0=out_real[..., 1])
             return fg_view_as_complex(out_real).to(torch.result_type(A, B))
         # 3) A real, B complex
         else:  # not A_is_complex and B_is_complex
-            Br = torch.view_as_real(B)
-            Ar = A.unsqueeze(-1) if isinstance(A, torch.Tensor) else A
-            if isinstance(Ar, torch.Tensor):
-                out_real = mul_func(Ar, Br)  # shape broadcasting requires Ar and Br
+            br, bi = B.real, B.imag
+            out_real = torch.empty((*br.shape, 2), dtype=br.dtype, device=br.device)
+            if isinstance(A, torch.Tensor):
+                mul_func(A, br, out0=out_real[..., 0])
+                mul_func(A, bi, out0=out_real[..., 1])
             else:
-                out_real = mul_func_scalar(Br, Ar)  # Br is tensor, Ar is scalar
+                mul_func_scalar(br, A, out0=out_real[..., 0])
+                mul_func_scalar(bi, A, out0=out_real[..., 1])
             return fg_view_as_complex(out_real).to(torch.result_type(A, B))
     elif isinstance(A, torch.Tensor) and isinstance(B, torch.Tensor):
         return mul_func(A, B)
