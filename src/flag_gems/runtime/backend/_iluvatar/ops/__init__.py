c5d4b1a0a898cb0d49572f881fe44c2d15f4674c
ftgreat <ldwang@baai.ac.cn>
2026-03-30 03:13:42 +0000

    [kernelgen2.0] Register repeat and ones operators in _iluvatar backend
    - Register repeat operator for Iluvatar platform
    - Register ones operator (generated previously)

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -2,7 +2,9 @@ from .add import add, add_
 from .div import div_mode, div_mode_
 from .exponential_ import exponential_
 from .mul import mul
+from .ones import ones
 from .pow import pow_scalar, pow_scalar_
+from .repeat import repeat
 from .sub import sub, sub_
 from .true_divide import true_divide, true_divide_out, true_divide_
 
@@ -13,8 +15,10 @@ __all__ = [
     "div_mode_",
     "exponential_",
     "mul",
+    "ones",
     "pow_scalar",
     "pow_scalar_",
+    "repeat",
     "sub",
     "sub_",
     "true_divide",
