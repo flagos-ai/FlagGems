29045ebbcb3806a3c18d3857a800fd41464749c8
XDYuanzhuLee <XDYuanzhuLee@users.noreply.github.com>
2026-04-15 16:04:45 +0800

    fix: format code, remove test dir, and fix linting

--- src/flag_gems/runtime/backend/_kunlunxin/ops/dot.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/dot.py
@@ -1,4 +1,5 @@
 import logging
+import math
 
 import torch
 import triton
@@ -28,23 +29,6 @@ def dot_kernel(x_ptr, y_ptr, out_ptr, N, BLOCK_SIZE: tl.constexpr):
 
 
 @libentry()
-@triton.autotune(
-    configs=[
-        triton.Config({"BLOCK_SIZE": 2048}, num_warps=4, num_stages=2),
-        triton.Config({"BLOCK_SIZE": 2048}, num_warps=8, num_stages=2),
-        triton.Config({"BLOCK_SIZE": 4096}, num_warps=4, num_stages=2),
-        triton.Config({"BLOCK_SIZE": 4096}, num_warps=8, num_stages=2),
-        triton.Config({"BLOCK_SIZE": 4096}, num_warps=16, num_stages=2),
-        triton.Config({"BLOCK_SIZE": 8192}, num_warps=4, num_stages=2),
-        triton.Config({"BLOCK_SIZE": 8192}, num_warps=8, num_stages=2),
-        triton.Config({"BLOCK_SIZE": 8192}, num_warps=16, num_stages=2),
-        triton.Config({"BLOCK_SIZE": 16384}, num_warps=8, num_stages=2),
-        triton.Config({"BLOCK_SIZE": 16384}, num_warps=16, num_stages=2),
-        triton.Config({"BLOCK_SIZE": 32768}, num_warps=8, num_stages=2),
-        triton.Config({"BLOCK_SIZE": 32768}, num_warps=16, num_stages=2),
-    ],
-    key=["N"],
-)
 @triton.jit
 def dot_kernel_1(x_ptr, y_ptr, mid_ptr, N, BLOCK_SIZE: tl.constexpr):
     pid = tle.program_id(0)
@@ -79,19 +63,23 @@ def dot(x, y):
 
     N = x.shape[0]
 
+    # Only when N is less than TRITON_MAX_TENSOR_NUMEL can it be processed with a single kernel,
+    # and performance is better when N < 4096
     if N >= 4096:
-        # Allocate for worst case (smallest block size = 2048)
-        max_mid_size = triton.cdiv(N, 2048)
-        block_mid = triton.next_power_of_2(max_mid_size)
+        block_size = triton.next_power_of_2(math.ceil(math.sqrt(N)))
 
-        grid_1 = lambda meta: (triton.cdiv(N, meta["BLOCK_SIZE"]),)
+        mid_size = triton.cdiv(N, block_size)
+        block_mid = triton.next_power_of_2(mid_size)
 
-        mid = torch.empty((max_mid_size,), dtype=torch.float32, device=x.device)
+        grid_1 = (mid_size, 1, 1)
+        grid_2 = (1, 1, 1)
+
+        mid = torch.empty((mid_size,), dtype=torch.float32, device=x.device)
         out = torch.empty([], dtype=x.dtype, device=x.device)
 
         with torch_device_fn.device(x.device):
-            dot_kernel_1[grid_1](x, y, mid, N)
-            dot_kernel_2[(1,)](mid, out, max_mid_size, block_mid)
+            dot_kernel_1[grid_1](x, y, mid, N, block_size)
+            dot_kernel_2[grid_2](mid, out, mid_size, block_mid)
 
     else:
         block_size = triton.next_power_of_2(N)
