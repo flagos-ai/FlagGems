a814f4b90a8505829899a1a16554f7f3430c516c
bin913 <842884726@qq.com>
2026-04-17 12:43:27 +0800

    fix ci test fail

--- tests/test_blas_ops.py
+++ tests/test_blas_ops.py
@@ -412,8 +412,8 @@ def test_grouped_mm(groups, N, K, dtype):
         dtype=torch.int32,
         device="cuda",
     )
-    ref_mat_a = to_reference(mat_a, True)
-    ref_mat_b = to_reference(mat_b, True)
+    ref_mat_a = to_reference(mat_a, False)
+    ref_mat_b = to_reference(mat_b, False)
     ref_out = torch._grouped_mm(ref_mat_a, ref_mat_b, offs)
     with flag_gems.use_gems():
         res_out = torch._grouped_mm(mat_a, mat_b, offs)
