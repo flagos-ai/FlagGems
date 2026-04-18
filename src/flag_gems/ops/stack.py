d7e9e9fe1faefaed0dd4a4a0fa71af8393647bc2
meinie <929449726@qq.com>
2026-04-18 17:42:44 +0800

    remove ..

--- src/flag_gems/ops/stack.py
+++ src/flag_gems/ops/stack.py
@@ -15,10 +15,6 @@ def stack_copy_func_kernel_4(
     in_ptr_b,
     in_ptr_c,
     in_ptr_d,
-    dim_size_in_a,
-    dim_size_in_b,
-    dim_size_in_c,
-    dim_size_in_d,
     dim_size_out,
     dim_prod_post,
     dim_offset_a,
@@ -36,22 +32,18 @@ def stack_copy_func_kernel_4(
 
     if pid_y == 0:
         in_ptr = in_ptr_a
-        dim_size_in = dim_size_in_a
         dim_offset = dim_offset_a
         total_elements = total_elements_a
     elif pid_y == 1:
         in_ptr = in_ptr_b
-        dim_size_in = dim_size_in_b
         dim_offset = dim_offset_b
         total_elements = total_elements_b
     elif pid_y == 2:
         in_ptr = in_ptr_c
-        dim_size_in = dim_size_in_c
         dim_offset = dim_offset_c
         total_elements = total_elements_c
     else:
         in_ptr = in_ptr_d
-        dim_size_in = dim_size_in_d
         dim_offset = dim_offset_d
         total_elements = total_elements_d
 
@@ -125,33 +117,29 @@ def stack(
             if j < num_tensors_in_batch:
                 tensor = tensors_in_batch[j].contiguous()
                 total_elements = tensor.numel()
-                args.extend([tensor, 1, i + j, total_elements])
+                args.extend([tensor, i + j, total_elements])
                 total_elements_list.append(total_elements)
             else:
-                args.extend([tensors_in_batch[0], 0, 0, 0])
+                args.extend([tensors_in_batch[0], 0, 0])
                 total_elements_list.append(0)
 
         dim_size_out = len(tensors)
 
         grid_y = num_tensors_in_batch
-        max_elements_in_batch = tensors[0].numel() if total_elements_list else 0
+        max_elements_in_batch = max(total_elements_list) if total_elements_list else 0
         grid = (triton.cdiv(max_elements_in_batch, BLOCK), grid_y)
 
         (
             tensor_a,
-            dim_size_in_a,
             dim_offset_a,
             total_elements_a,
             tensor_b,
-            dim_size_in_b,
             dim_offset_b,
             total_elements_b,
             tensor_c,
-            dim_size_in_c,
             dim_offset_c,
             total_elements_c,
             tensor_d,
-            dim_size_in_d,
             dim_offset_d,
             total_elements_d,
         ) = args
@@ -162,10 +150,6 @@ def stack(
             tensor_b,
             tensor_c,
             tensor_d,
-            dim_size_in_a,
-            dim_size_in_b,
-            dim_size_in_c,
-            dim_size_in_d,
             dim_size_out,
             dim_prod_post,
             dim_offset_a,
