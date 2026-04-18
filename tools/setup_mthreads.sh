5ab29ea72d461c0629b0b195799d8bf17a759d09
Qiming Teng <tengqm@outlook.com>
2026-04-18 14:44:42 +0800

    Fix mthreads dependency again and again (#2507)

--- tools/setup_mthreads.sh
+++ tools/setup_mthreads.sh
@@ -2,9 +2,11 @@
 
 uv pip install -e .[mthreads,test]
 
-# TODO: Drop the following line
-uv pip install --index $FLAGOS_PYPI \
-  torch==2.7.1+musa4.0.0
+# TODO: Drop the following lines
+uv pip install -v --index $FLAGOS_PYPI \
+  "torch==2.7.1+musa4.0.0"
+uv pip install -v --index $FLAGOS_PYPI \
+  "torch_musa==2.7.1"
 
 export MUSA_HOME=/usr/local/musa
 export PATH=$MUSA_HOME/bin:$PATH
