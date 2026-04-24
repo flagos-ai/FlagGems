72c72b89fb3aef9dfd1938c382f1c135ea66ccf8
Qiming Teng <tengqm@outlook.com>
2026-04-24 18:41:40 +0800

    Fix setup script again (#2645)

--- setup.sh
+++ setup.sh
@@ -101,7 +101,7 @@ fi
 # Install
 export FLAGOS_PYPI="https://resource.flagos.net/repository/flagos-pypi-${VENDOR}/simple"
 printf "Install build tools ... "
-uv pip install -q
+uv pip install -q \
   "setuptools>=64.0" \
   "scikit-build-core==0.12.2" \
   "pybind11==3.0.3" \
