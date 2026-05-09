d867ff9d38170da9ea618b8e744aa386e46c9b66
factnn <1050552884@qq.com>
2026-05-09 15:55:50 +0800

    fix: rename benchmark file, update to new format

--- benchmark/test_histc.py
+++ benchmark/test_histc.py
@@ -1,18 +1,18 @@
 import pytest
 import torch
 
-from benchmark.performance_utils import GenericBenchmark2DOnly
+from . import base
 
 
-def histc_input_fn(shape, dtype, device):
+def _input_fn(shape, dtype, device):
     inp = torch.rand(shape, dtype=dtype, device=device) * 10
     yield inp, {"bins": 100, "min": 0, "max": 10}
 
 
 @pytest.mark.histc
-def test_perf_histc():
-    bench = GenericBenchmark2DOnly(
-        input_fn=histc_input_fn,
+def test_histc():
+    bench = base.GenericBenchmark2DOnly(
+        input_fn=_input_fn,
         op_name="histc",
         torch_op=torch.histc,
         dtypes=[torch.float32],
