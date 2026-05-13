b26947d3f3b7c0d3f51ee1d295e0150b808f9648
taooo <gumptao2997@gmail.com>
2026-05-13 19:03:07 +0800

    Improve any_dims test coverage: 缺少性能测试,benchmark/test_any.py中有mark any但缺少mark any_dims

--- benchmark/test_any.py
+++ benchmark/test_any.py
@@ -10,3 +10,22 @@ def test_any():
         op_name="any", torch_op=torch.any, dtypes=consts.FLOAT_DTYPES
     )
     bench.run()
+
+
+def any_dims_input_fn(shape, dtype, device):
+    inp = torch.randn(shape, dtype=dtype, device=device)
+    if inp.ndim >= 2:
+        yield inp, {"dim": [0, 1]}
+    else:
+        yield inp, {"dim": 0}
+
+
+@pytest.mark.any_dims
+def test_any_dims():
+    bench = base.GenericBenchmarkExcluse1D(
+        op_name="any_dims",
+        torch_op=torch.any,
+        input_fn=any_dims_input_fn,
+        dtypes=consts.FLOAT_DTYPES,
+    )
+    bench.run()
