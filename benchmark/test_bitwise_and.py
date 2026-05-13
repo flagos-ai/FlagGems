c44e0b1b3dbfdc03356458a7573d51ced2247e55
taooo <gumptao2997@gmail.com>
2026-05-13 23:16:12 +0800

    Improve bitwise_and_scalar_ test coverage: 缺少性能测试,benchmark/test_bitwise_and.py中有mark:['bitwise_and_tensor', 'bitwise_and_tensor_'],缺少mark'bitwise_and_scalar_'

--- benchmark/test_bitwise_and.py
+++ benchmark/test_bitwise_and.py
@@ -23,3 +23,24 @@ def test_bitwise_and_inplace():
         is_inplace=True,
     )
     bench.run()
+
+
+def bitwise_and_scalar_input_fn(shape, cur_dtype, device):
+    inp1 = base.generate_tensor_input(shape, cur_dtype, device)
+    if cur_dtype == torch.bool:
+        inp2 = True
+    else:
+        inp2 = 0x00FF
+    yield inp1, inp2
+
+
+@pytest.mark.bitwise_and_scalar_
+def test_bitwise_and_scalar_():
+    bench = base.GenericBenchmark(
+        op_name="bitwise_and_scalar_",
+        torch_op=lambda a, b: a.bitwise_and_(b),
+        dtypes=consts.INT_DTYPES + consts.BOOL_DTYPES,
+        input_fn=bitwise_and_scalar_input_fn,
+        is_inplace=True,
+    )
+    bench.run()
