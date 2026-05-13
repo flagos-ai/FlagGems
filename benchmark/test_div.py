edc1e6358ff9ea769174e1a5c41de377af8bd59a
taooo <gumptao2997@gmail.com>
2026-05-13 23:28:58 +0800

    Improve div_scalar test coverage: add missing div_scalar benchmark

--- benchmark/test_div.py
+++ benchmark/test_div.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from . import base, consts
+from . import base, consts, utils
 
 
 # TODO(0x45f): Fix OOM when dtypes includes COMPLEX_DTYPES (Issue #2693).
@@ -22,5 +22,21 @@ def test_div_inplace():
         torch_op=lambda a, b: a.div_(b),
         dtypes=consts.FLOAT_DTYPES,
         is_inplace=True,
+    )
+    bench.run()
+
+
+def _scalar_input_fn(shape, dtype, device):
+    inp = utils.generate_tensor_input(shape, dtype, device)
+    yield inp, 0.5
+
+
+@pytest.mark.div_scalar
+def test_div_scalar():
+    bench = base.GenericBenchmark(
+        input_fn=_scalar_input_fn,
+        op_name="div_scalar",
+        torch_op=torch.div,
+        dtypes=consts.FLOAT_DTYPES,
     )
     bench.run()
