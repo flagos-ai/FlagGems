0c5782ea409b0c3cd63d82b5b6284949f9f80c9b
taooo <gumptao2997@gmail.com>
2026-05-13 19:02:21 +0800

    Improve arange_start_step test coverage: 缺少性能测试,benchmark/test_arange.py中有mark arange但缺少mark arange_start_step

--- benchmark/test_arange.py
+++ benchmark/test_arange.py
@@ -29,3 +29,30 @@ def test_arange():
         op_name="arange", input_fn=_input_fn, torch_op=torch.arange
     )
     bench.run()
+
+
+def _input_fn_start_step(shape, dtype, device):
+    yield {
+        "start": 0,
+        "end": math.prod(shape),
+        "step": 1,
+        "device": device,
+        "dtype": dtype,
+    },
+
+    if base.Config.bench_level == consts.BenchLevel.COMPREHENSIVE:
+        yield {
+            "start": 10,
+            "end": math.prod(shape) + 10,
+            "step": 2,
+            "device": device,
+            "dtype": dtype,
+        },
+
+
+@pytest.mark.arange_start_step
+def test_arange_start_step():
+    bench = base.GenericBenchmark(
+        op_name="arange_start_step", input_fn=_input_fn_start_step, torch_op=torch.arange
+    )
+    bench.run()
