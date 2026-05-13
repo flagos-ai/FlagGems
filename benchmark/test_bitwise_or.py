f5e2d26fe6894a818cb9f7645099a7e3cd0cb7ca
taooo <gumptao2997@gmail.com>
2026-05-13 23:13:41 +0800

    Improve bitwise_or_scalar_ test coverage: add missing benchmark with mark 'bitwise_or_scalar_'

--- benchmark/test_bitwise_or.py
+++ benchmark/test_bitwise_or.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from . import base, consts
+from . import base, consts, utils
 
 
 @pytest.mark.bitwise_or_tensor
@@ -21,5 +21,22 @@ def test_bitwise_or_inplace():
         torch_op=lambda a, b: a.bitwise_or_(b),
         dtypes=consts.INT_DTYPES + consts.BOOL_DTYPES,
         is_inplace=True,
+    )
+    bench.run()
+
+
+def _scalar_input_fn(shape, dtype, device):
+    inp = utils.generate_tensor_input(shape, dtype, device)
+    yield inp, 0x5A
+
+
+@pytest.mark.bitwise_or_scalar_
+def test_bitwise_or_scalar_():
+    bench = base.GenericBenchmark(
+        input_fn=_scalar_input_fn,
+        op_name="bitwise_or_scalar_",
+        torch_op=torch.Tensor.bitwise_or_,
+        dtypes=consts.INT_DTYPES + consts.BOOL_DTYPES,
+        inplace=True,
     )
     bench.run()
