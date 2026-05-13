5fe509c3b9fcb46044e131911a4f013fa702851f
taooo <gumptao2997@gmail.com>
2026-05-14 00:10:54 +0800

    Improve pow_tensor_scalar_ test coverage: add missing benchmark with mark 'pow_tensor_scalar_'

--- benchmark/test_pow.py
+++ benchmark/test_pow.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from . import base, consts
+from . import base, consts, utils
 
 
 @pytest.mark.pow_tensor_tensor
@@ -19,6 +19,23 @@ def test_pow_inplace():
     bench = base.BinaryPointwiseBenchmark(
         op_name="pow_tensor_tensor_",
         torch_op=lambda a, b: a.pow_(b),
+        dtypes=consts.FLOAT_DTYPES,
+        is_inplace=True,
+    )
+    bench.run()
+
+
+def _pow_tensor_scalar_input_fn(shape, dtype, device):
+    inp = utils.generate_tensor_input(shape, dtype, device)
+    yield inp, 2.0
+
+
+@pytest.mark.pow_tensor_scalar_
+def test_pow_tensor_scalar_inplace():
+    bench = base.GenericBenchmark(
+        op_name="pow_tensor_scalar_",
+        torch_op=lambda a, b: a.pow_(b),
+        input_fn=_pow_tensor_scalar_input_fn,
         dtypes=consts.FLOAT_DTYPES,
         is_inplace=True,
     )
