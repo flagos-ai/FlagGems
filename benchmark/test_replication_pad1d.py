22a179a544a57b228e96d8538814f3a6772d0fd6
taooo <gumptao2997@gmail.com>
2026-05-14 00:12:56 +0800

    Improve replication_pad1d_out test coverage: add missing benchmark with mark 'replication_pad1d_out'

--- benchmark/test_replication_pad1d.py
+++ benchmark/test_replication_pad1d.py
@@ -36,3 +36,43 @@ def test_replication_pad1d():
     )
 
     bench.run()
+
+
+def _torch_replication_pad1d_out(x, padding):
+    pad_left, pad_right = padding[0], padding[1]
+    if x.dim() == 3:
+        N, C, W_in = x.shape
+        out_shape = (N, C, W_in + pad_left + pad_right)
+    else:
+        C, W_in = x.shape
+        out_shape = (C, W_in + pad_left + pad_right)
+    out = torch.empty(out_shape, dtype=x.dtype, device=x.device)
+    return torch.ops.aten.replication_pad1d.out(x, padding, out=out)
+
+
+class ReplicationPad1dOutBenchmark(base.Benchmark):
+    def set_shapes(self, shape_file_path=None):
+        self.shapes = [
+            ((2, 3, 7), (1, 2)),
+            ((4, 16, 64), (3, 1)),
+            ((8, 32, 256), (1, 2)),
+            ((32, 256), (3, 1)),
+        ]
+
+    def set_more_shapes(self):
+        return []
+
+    def get_input_iter(self, dtype):
+        for config in self.shapes:
+            yield from _input_fn(config, dtype, self.device)
+
+
+@pytest.mark.replication_pad1d_out
+def test_replication_pad1d_out():
+    bench = ReplicationPad1dOutBenchmark(
+        op_name="replication_pad1d_out",
+        torch_op=_torch_replication_pad1d_out,
+        dtypes=consts.FLOAT_DTYPES,
+    )
+
+    bench.run()
