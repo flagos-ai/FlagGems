24145d5c45539ef52320f5c6510238df03170c73
taooo <gumptao2997@gmail.com>
2026-05-13 23:15:47 +0800

    Improve bitwise_or_scalar_tensor test coverage: 缺少性能测试,benchmark/test_bitwise_or.py中有mark:['bitwise_or_tensor', 'bitwise_or_tensor_'],缺少mark'bitwise_or_scalar_tensor'

--- benchmark/test_bitwise_or.py
+++ benchmark/test_bitwise_or.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from . import base, consts
+from . import base, consts, utils
 
 
 @pytest.mark.bitwise_or_tensor
@@ -21,5 +21,22 @@ def test_bitwise_or_inplace():
         torch_op=lambda a, b: a.bitwise_or_(b),
         dtypes=consts.INT_DTYPES + consts.BOOL_DTYPES,
         is_inplace=True,
+    )
+    bench.run()
+
+
+def scalar_tensor_input_fn(shape, cur_dtype, device):
+    scalar = 0x00FF if cur_dtype != torch.bool else True
+    tensor = utils.generate_tensor_input(shape, cur_dtype, device)
+    yield scalar, tensor
+
+
+@pytest.mark.bitwise_or_scalar_tensor
+def test_bitwise_or_scalar_tensor():
+    bench = base.GenericBenchmark(
+        op_name="bitwise_or_scalar_tensor",
+        torch_op=torch.bitwise_or,
+        dtypes=consts.INT_DTYPES + consts.BOOL_DTYPES,
+        input_fn=scalar_tensor_input_fn,
     )
     bench.run()
