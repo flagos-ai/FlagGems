991fea99ffc9f264073e3e7f04a31790a556cd32
Schopenhauer-loves-Hegel <Schopenhauer-loves-Hegel@users.noreply.github.com>
2026-05-15 03:56:48 +0000

    Improve any test coverage
    Merge PRs:
    - #3087: Add any_dim benchmark and test
    - #3088: Add any_dims benchmark test

--- benchmark/test_any.py
+++ benchmark/test_any.py
@@ -10,3 +10,30 @@ def test_any():
         op_name="any", torch_op=torch.any, dtypes=consts.FLOAT_DTYPES
     )
     bench.run()
+
+
+@pytest.mark.any_dim
+def test_any_dim():
+    bench = base.UnaryReductionBenchmark(
+        op_name="any_dim", torch_op=torch.any, dtypes=consts.FLOAT_DTYPES
+    )
+    bench.run()
+
+
+def any_dims_input_fn(shape, dtype, device):
+    inp = torch.randn(shape, dtype=dtype, device=device)
+    if inp.ndim >= 2:
+        yield inp, {"dim": [0, 1]}
+    else:
+        yield inp, {"dim": 0}
+
+
+@pytest.mark.any_dims
+def test_any_dims():
+    bench = base.GenericBenchmarkExcluse1D(
+        op_name="any_dims",
+        torch_op=torch.any,
+        input_fn=any_dims_input_fn,
+        dtypes=consts.FLOAT_DTYPES,
+    )
+    bench.run()
