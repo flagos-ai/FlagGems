7b51776ac655b61de6fd866b732c488a065be4ed
gittare <24656863+gittare@users.noreply.github.com>
2026-05-18 20:59:52 +0300

    Update __init__.py
    Signed-off-by: gittare <24656863+gittare@users.noreply.github.com>

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -3,6 +3,7 @@ from flag_gems.ops._functional_sym_constrain_range_for_size import (
     _functional_sym_constrain_range_for_size,
 )
 from flag_gems.ops._safe_softmax import _safe_softmax
+from flag_gems.experimental_ops import _assert_async
 from flag_gems.ops._upsample_nearest_exact1d import _upsample_nearest_exact1d
 from flag_gems.ops.abs import abs, abs_
 from flag_gems.ops.absolute import absolute
