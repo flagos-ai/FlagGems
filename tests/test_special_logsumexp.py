3fbc971439a45270b8cef4aac50d6f8c4503bcf8
XDYuanzhuLee <2680679588@qq.com>
2026-05-20 08:10:28 +0000

    [KernelGen][Nvidia] Add special_logsumexp operator with Triton kernel

---
+++ tests/test_special_logsumexp.py
@@ -0,0 +1,22 @@
+import pytest
+import torch
+
+import flag_gems
+
+from . import accuracy_utils as utils
+
+
+@pytest.mark.special_logsumexp
+@pytest.mark.parametrize("shape", utils.REDUCTION_SHAPES)
+@pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
+@pytest.mark.parametrize("dim", [0, 1])
+@pytest.mark.parametrize("keepdim", [True, False])
+def test_special_logsumexp(shape, dtype, dim, keepdim):
+    inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    ref_inp = utils.to_reference(inp, True)
+
+    ref_out = torch.special.logsumexp(ref_inp, dim=dim, keepdim=keepdim)
+    with flag_gems.use_gems():
+        res_out = torch.special.logsumexp(inp, dim=dim, keepdim=keepdim)
+
+    utils.gems_assert_close(res_out, ref_out, dtype)
