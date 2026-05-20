1e9b7f1c51212ffc758caafe4a3233f0fba1326b
XDYuanzhuLee <2680679588@qq.com>
2026-05-20 08:10:28 +0000

    [KernelGen][Nvidia] Add special_logsumexp operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -467,6 +467,7 @@ _FULL_CONFIG = (
     ("special_i0e.out", special_i0e_out),
     ("special_i1", special_i1),
     ("special_i1.out", special_i1_out),
+    ("special_logsumexp", special_logsumexp),
     ("sqrt", sqrt),
     ("sqrt_", sqrt_),
     ("square", square),
--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -323,6 +323,7 @@ from flag_gems.ops.softshrink import softshrink, softshrink_out
 from flag_gems.ops.sort import sort, sort_stable
 from flag_gems.ops.special_i0e import special_i0e, special_i0e_out
 from flag_gems.ops.special_i1 import special_i1, special_i1_out
+from flag_gems.ops.special_logsumexp import logsumexp as special_logsumexp
 from flag_gems.ops.sqrt import sqrt, sqrt_
 from flag_gems.ops.square import square, square_, square_out
 from flag_gems.ops.stack import stack
@@ -779,6 +780,7 @@ __all__ = [
     "special_i1_out",
     "special_i0e",
     "special_i0e_out",
+    "special_logsumexp",
     "sqrt",
     "sqrt_",
     "square",
