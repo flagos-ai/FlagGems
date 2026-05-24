fc3ee37ba0773e1debaba6c87d693010122a78d5
XDYuanzhuLee <2680679588@qq.com>
2026-05-24 04:02:53 +0000

    [KernelGen][Nvidia] Fix bernoulli: keep upstream tests, add statistical validation, fix func names

--- tests/test_bernoulli.py
+++ tests/test_bernoulli.py
@@ -5,34 +5,6 @@ import flag_gems
 
 from . import accuracy_utils as utils
 
-SPECIAL_SHAPES = utils.POINTWISE_SHAPES
-
-
-@pytest.mark.bernoulli
-@pytest.mark.parametrize("shape", SPECIAL_SHAPES)
-@pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
-def test_bernoulli(shape, dtype):
-    inp = torch.rand(shape, dtype=dtype, device=flag_gems.device)
-    with flag_gems.use_gems():
-        res_out = torch.bernoulli(inp)
-    assert (res_out == 0).logical_or(res_out == 1).all()
-    assert res_out.shape == shape
-
-
-# bfloat16 excluded: deterministic boundary test only needs float16+float32
-@pytest.mark.bernoulli
-@pytest.mark.parametrize("dtype", [torch.float16, torch.float32])
-def test_bernoulli_deterministic(dtype):
-    inp_zeros = torch.zeros(1024, dtype=dtype, device=flag_gems.device)
-    with flag_gems.use_gems():
-        out_zeros = torch.bernoulli(inp_zeros)
-    assert (out_zeros == 0).all()
-
-    inp_ones = torch.ones(1024, dtype=dtype, device=flag_gems.device)
-    with flag_gems.use_gems():
-        out_ones = torch.bernoulli(inp_ones)
-    assert (out_ones == 1).all()
-
 
 @pytest.mark.bernoulli_
 @pytest.mark.parametrize("shape", utils.DISTRIBUTION_SHAPES)
@@ -43,8 +15,10 @@ def test_bernoulli_(shape, dtype):
     with flag_gems.use_gems():
         x.bernoulli_(p)
 
+    # Check that all values are 0 or 1
     assert ((x == 0) | (x == 1)).all()
 
+    # Check that the mean is approximately p (statistical test)
     mean = x.float().mean().item()
     assert abs(mean - p) < 0.1
 
@@ -58,12 +32,32 @@ def test_bernoulli_various_p(shape, dtype, p):
     with flag_gems.use_gems():
         x.bernoulli_(p)
 
+    # Check that all values are 0 or 1
     assert ((x == 0) | (x == 1)).all()
 
+    # Check boundary cases
     if p == 0.0:
         assert (x == 0).all()
     elif p == 1.0:
         assert (x == 1).all()
     else:
+        # Check that the mean is approximately p
         mean = x.float().mean().item()
         assert abs(mean - p) < 0.15
+
+
+@pytest.mark.bernoulli
+@pytest.mark.parametrize("shape", utils.DISTRIBUTION_SHAPES)
+@pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
+def test_bernoulli(shape, dtype):
+    inp = torch.rand(shape, dtype=dtype, device=flag_gems.device)
+    with flag_gems.use_gems():
+        res_out = torch.bernoulli(inp)
+
+    # Check that all values are 0 or 1
+    assert ((res_out == 0) | (res_out == 1)).all()
+
+    # Statistical check: mean of output should approximate mean of input probabilities
+    expected_mean = inp.float().mean().item()
+    actual_mean = res_out.float().mean().item()
+    assert abs(actual_mean - expected_mean) < 0.1
