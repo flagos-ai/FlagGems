5f44817db6a9d05df219a77ad3ab074529cacbce
XDYuanzhuLee <2680679588@qq.com>
2026-05-24 05:14:46 +0000

    [KernelGen][Nvidia] Fix device mismatch in quick-cpu test: use to_reference for all inputs

--- tests/test_unsafe_masked_index_put_accumulate.py
+++ tests/test_unsafe_masked_index_put_accumulate.py
@@ -21,19 +21,19 @@ _UNSAFE_MASKED_INDEX_PUT_DTYPES = [torch.float16, torch.float32]
 @pytest.mark.parametrize("dtype", _UNSAFE_MASKED_INDEX_PUT_DTYPES)
 def test_unsafe_masked_index_put_accumulate(shape, dtype):
     inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
-    ref_inp = utils.to_reference(inp)
-
-    mask = torch.rand(shape) < 0.3
-    mask = mask.to(flag_gems.device)
-
+    mask = (torch.rand(shape, device=flag_gems.device) < 0.3).bool()
     indices = torch.randint(
         0, max(shape[-1], 1), shape, dtype=torch.long, device=flag_gems.device
     )
-
     values = torch.randn(shape, dtype=dtype, device=flag_gems.device)
 
+    ref_inp = utils.to_reference(inp)
+    ref_mask = utils.to_reference(mask)
+    ref_indices = utils.to_reference(indices)
+    ref_values = utils.to_reference(values)
+
     ref_out = torch._unsafe_masked_index_put_accumulate(
-        ref_inp, mask, (indices,), values
+        ref_inp, ref_mask, (ref_indices,), ref_values
     )
     with flag_gems.use_gems():
         res_out = torch._unsafe_masked_index_put_accumulate(
