9f0ba3870f0e7e63ba39d52564e91022e2d35bbd
XDYuanzhuLee <2680679588@qq.com>
2026-05-24 04:01:13 +0000

    [KernelGen][Nvidia] Simplify bessel_y1 test: use gems_assert_close(equal_nan=True)

--- tests/test_special_bessel_y1.py
+++ tests/test_special_bessel_y1.py
@@ -11,26 +11,10 @@ from . import accuracy_utils as utils
 @pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
 def test_special_bessel_y1(shape, dtype):
     inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
-    ref_inp = utils.to_reference(inp)
+    ref_inp = utils.to_reference(inp, True)
 
-    # Compute reference in float32 since torch.special.bessel_y1 doesn't support float16
-    ref_inp_f32 = ref_inp.to(torch.float32)
-    ref_out = torch.special.bessel_y1(ref_inp_f32).to(dtype)
+    ref_out = torch.special.bessel_y1(ref_inp)
     with flag_gems.use_gems():
         res_out = torch.special.bessel_y1(inp)
 
-    # Bessel Y1 produces NaN for negative inputs and -inf for large inputs;
-    # handle comparison by checking special values first
-    res_cpu = res_out.to("cpu")
-    ref_cpu = ref_out.to("cpu")
-    res_inf = torch.isinf(res_cpu)
-    ref_inf = torch.isinf(ref_cpu)
-    res_nan = torch.isnan(res_cpu)
-    ref_nan = torch.isnan(ref_cpu)
-    # Check that inf/-inf and NaN positions match
-    assert (res_inf == ref_inf).all(), "Inf positions don't match"
-    assert (res_nan == ref_nan).all(), "NaN positions don't match"
-    # For finite values, check closeness
-    valid_mask = ~res_inf & ~ref_inf & ~res_nan & ~ref_nan
-    if valid_mask.sum() > 0:
-        utils.gems_assert_close(res_cpu[valid_mask], ref_cpu[valid_mask], dtype)
+    utils.gems_assert_close(res_out, ref_out, dtype, equal_nan=True)
