337ef900eb7a0be6c7cbabb4371c6b67580f143f
jack <hellojack163@gmail.com>
2026-05-25 13:56:23 +0800

    [KMCompiler]added the pack_seq_triton op (#3433)
    * added the pack_seq op
    
    * deleted the unpack_seq_triton

--- src/flag_gems/fused/__init__.py
+++ src/flag_gems/fused/__init__.py
@@ -42,6 +42,7 @@ from flag_gems.fused.moe_align_block_size import (
 )
 from flag_gems.fused.moe_sum import moe_sum
 from flag_gems.fused.outer import outer
+from flag_gems.fused.pack_seq import pack_seq_triton
 from flag_gems.fused.reglu import dreglu, reglu
 from flag_gems.fused.reshape_and_cache import reshape_and_cache
 from flag_gems.fused.reshape_and_cache_flash import reshape_and_cache_flash
@@ -99,6 +100,7 @@ __all__ = [
     "moe_align_block_size_triton",
     "outer",
     "outplace_fused_experts",
+    "pack_seq_triton",
     "reglu",
     "reshape_and_cache",
     "reshape_and_cache_flash",
