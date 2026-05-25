83e325cc02cbb804342ebe4237036394acbc10d0
XDYuanzhuLee <2680679588@qq.com>
2026-05-25 06:25:48 +0000

    [KernelGen] Fix rad2deg: split yaml into rad2deg + rad2deg_, add inplace benchmark

--- benchmark/test_rad2deg.py
+++ benchmark/test_rad2deg.py
@@ -12,3 +12,13 @@ def test_rad2deg():
         dtypes=consts.FLOAT_DTYPES,
     )
     bench.run()
+
+
+@pytest.mark.rad2deg_
+def test_rad2deg_():
+    bench = base.UnaryPointwiseBenchmark(
+        op_name="rad2deg_",
+        torch_op=torch.Tensor.rad2deg_,
+        dtypes=consts.FLOAT_DTYPES,
+    )
+    bench.run()
