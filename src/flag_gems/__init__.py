f2c8a89cbd42421dd0d20a20ca9a59ca620e7e80
XDYuanzhuLee <2680679588@qq.com>
2026-05-26 09:31:30 +0000

    [KernelGen][Nvidia] Add renorm operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -428,6 +428,7 @@ _FULL_CONFIG = (
     ("remainder.Tensor", remainder),
     ("remainder_.Scalar", remainder_),
     ("remainder_.Tensor", remainder_),
+    ("renorm", renorm),
     ("repeat", repeat),
     ("repeat_interleave.self_int", repeat_interleave_self_int),
     ("repeat_interleave.self_Tensor", repeat_interleave_self_tensor),
