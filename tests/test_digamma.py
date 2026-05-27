3a2f9944891fcfa7519a9c675e6e541de8224933
LoserCheems <losercheems@gmail.com>
2026-05-27 16:56:01 +0800

    Simplify test_digamma to match digamma_ test style, use standard gems_assert_close

--- tests/test_digamma.py
+++ tests/test_digamma.py
@@ -10,19 +10,11 @@ from . import accuracy_utils as utils
 @pytest.mark.parametrize("shape", utils.POINTWISE_SHAPES)
 @pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
 def test_digamma(shape, dtype):
-    # Use positive inputs to avoid extreme values and NaN at poles
     inp = torch.rand(shape, dtype=dtype, device=flag_gems.device) + 1.0
     ref_inp = utils.to_reference(inp)
+
     ref_out = torch.digamma(ref_inp)
     with flag_gems.use_gems():
         res_out = torch.digamma(inp)
 
-    # Use more lenient tolerance for digamma due to its wide range of output values
-    if dtype == torch.float16:
-        # Float16 needs higher tolerance due to precision loss from float32 computation
-        utils.gems_assert_close(res_out, ref_out, dtype, atol=1e-2)
-    elif dtype == torch.bfloat16:
-        # Bfloat16 needs even higher tolerance due to its lower precision
-        utils.gems_assert_close(res_out, ref_out, dtype, atol=1e-1)
-    else:
-        utils.gems_assert_close(res_out, ref_out, dtype)
+    utils.gems_assert_close(res_out, ref_out, dtype)
