eaa3ed8ca0c075bba42b6832ff3f6742be5d7a22
Adam Platin <AdamPlatin123@outlook.com>
2026-05-27 23:27:48 +0800

    Add benchmark and test for log_sigmoid_backward

--- benchmark/test_log_sigmoid.py
+++ benchmark/test_log_sigmoid.py
@@ -12,3 +12,13 @@ def test_log_sigmoid():
         dtypes=consts.FLOAT_DTYPES,
     )
     bench.run()
+
+
+@pytest.mark.log_sigmoid_backward
+def test_log_sigmoid_backward():
+    bench = base.UnaryPointwiseBenchmark(
+        op_name="log_sigmoid_backward",
+        torch_op=torch.ops.aten.log_sigmoid_backward,
+        dtypes=consts.FLOAT_DTYPES,
+    )
+    bench.run()
