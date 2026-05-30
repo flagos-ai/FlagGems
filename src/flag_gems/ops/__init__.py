ce9340cc779af953ff96c34b40018d875ccce5e8
Adam Platin <AdamPlatin123@outlook.com>
2026-05-30 15:07:55 +0800

    [Track 1-B] Add mish and mish_backward operators
    - mish(x) = x * tanh(softplus(x))
    - mish_backward uses @pointwise_dynamic pattern
    - Add test and benchmark files

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -236,6 +236,7 @@ from flag_gems.ops.mean import mean, mean_dim
 from flag_gems.ops.median import median, median_dim, median_dim_values, median_out
 from flag_gems.ops.min import min, min_dim
 from flag_gems.ops.minimum import minimum
+from flag_gems.ops.mish import mish, mish_backward
 from flag_gems.ops.mm import mm, mm_out, router_gemm
 from flag_gems.ops.mse_loss import mse_loss
 from flag_gems.ops.mul import mul, mul_
@@ -705,6 +706,8 @@ __all__ = [
     "min",
     "min_dim",
     "minimum",
+    "mish",
+    "mish_backward",
     "mm",
     "mm_out",
     "mse_loss",
