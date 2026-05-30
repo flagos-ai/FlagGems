43955cbb860194d33a96a72d04969c247ed00515
XDYuanzhuLee <2680679588@qq.com>
2026-05-30 05:30:06 +0000

    Fix review: centralize PERMUTE_COPY_SHAPES_AND_DIMS to accuracy_utils.py

--- tests/test_permute_copy.py
+++ tests/test_permute_copy.py
@@ -5,22 +5,7 @@ import flag_gems
 
 from . import accuracy_utils as utils
 
-PERMUTE_COPY_SHAPES_AND_DIMS = [
-    # 1D cases
-    ((24,), (0,)),
-    ((12,), (0,)),
-    # 2D cases
-    ((2, 3), (0, 1)),
-    ((2, 3), (1, 0)),
-    ((3, 4), (1, 0)),
-    ((16, 128), (1, 0)),
-    # 3D cases (from original PERMUTE_COPY_SHAPES x PERMUTE_COPY_PERMS)
-    ((2, 3, 4), (0, 2, 1)),
-    ((2, 3, 4), (1, 0, 2)),
-    ((2, 3, 4), (2, 1, 0)),
-    ((4, 2, 3), (1, 2, 0)),
-    ((4, 2, 3), (2, 0, 1)),
-]
+PERMUTE_COPY_SHAPES_AND_DIMS = utils.PERMUTE_COPY_SHAPES_AND_DIMS
 
 
 @pytest.mark.permute_copy
