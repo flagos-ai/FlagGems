c07d5e9144263e132afceb6f237f4bc86969d314
Jennie1004 <141099828+Jennie1004@users.noreply.github.com>
2026-06-01 17:28:43 +0800

    add flash_mla_with_kvcache op (#3659)

--- src/flag_gems/fused/__init__.py
+++ src/flag_gems/fused/__init__.py
@@ -24,6 +24,7 @@ from flag_gems.fused.FLA import (
     fused_recurrent_gated_delta_rule_fwd,
 )
 from flag_gems.fused.flash_mla import flash_mla
+from flag_gems.fused.flash_mla_with_kvcache import flash_mla_with_kvcache
 from flag_gems.fused.flashmla_sparse import flash_mla_sparse_fwd
 from flag_gems.fused.fused_add_rms_norm import fused_add_rms_norm
 from flag_gems.fused.fused_deepseek_v4_qnorm_rope_kv_rope_quant_insert import (
@@ -100,6 +101,7 @@ __all__ = [
     "dswiglu",
     "flash_mla",
     "flash_mla_sparse_fwd",
+    "flash_mla_with_kvcache",
     "fused_add_rms_norm",
     "fused_deepseek_v4_qnorm_rope_kv_rope_quant_insert",
     "fused_experts_impl",
