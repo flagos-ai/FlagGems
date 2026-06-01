f426a1626e7bb816d16ef7a9fbba630ba1577f8b
XDYuanzhuLee <2680679588@qq.com>
2026-06-01 07:58:42 +0000

    [KernelGen][Nvidia] Add BatchNorm operator with Muxi and Tianshu domestic backends

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -1,9 +1,11 @@
+from .batch_norm import batch_norm
 from .div import div_mode, div_mode_
 from .hadamard_transform import hadamard_transform
 from .matmul_bf16 import matmul_bf16
 from .matmul_int8 import matmul_int8
 
 __all__ = [
+    "batch_norm",
     "div_mode",
     "div_mode_",
     "hadamard_transform",
