34d94f4f1d011a575fb4f04e61f2a01d145f8d1b
Adam Platin <AdamPlatin123@outlook.com>
2026-06-03 20:58:17 +0800

    [FlagGems Operator Development Competition] Add replication_pad1d_backward operator

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -71,7 +71,7 @@ from flag_gems.ops.bmm import bmm, bmm_out
 from flag_gems.ops.cat import cat, cat_out
 from flag_gems.ops.cauchy import cauchy, cauchy_
 from flag_gems.ops.ceil import ceil, ceil_, ceil_out
-from flag_gems.ops.celu import celu, celu_
+from flag_gems.ops.celu import celu, celu_, celu_backward
 from flag_gems.ops.clamp import (
     clamp,
     clamp_,
@@ -173,7 +173,10 @@ from flag_gems.ops.hadamard_transform import (
     hadamard_transform_28N,
     hadamard_transform_40N,
 )
+from flag_gems.ops.hardshrink import hardshrink
+from flag_gems.ops.hardshrink_backward import hardshrink_backward
 from flag_gems.ops.hardsigmoid import hardsigmoid, hardsigmoid_out
+from flag_gems.ops.hardsigmoid_backward import hardsigmoid_backward
 from flag_gems.ops.hardswish_ import hardswish_
 from flag_gems.ops.histc import histc
 from flag_gems.ops.hstack import hstack
@@ -216,6 +219,7 @@ from flag_gems.ops.logical_or import logical_or, logical_or_
 from flag_gems.ops.logical_xor import logical_xor
 from flag_gems.ops.logit import logit, logit_out
 from flag_gems.ops.logit_ import logit_
+from flag_gems.ops.logit_backward import logit_backward
 from flag_gems.ops.logspace import logspace
 from flag_gems.ops.logsumexp import logsumexp
 from flag_gems.ops.lt import lt, lt_scalar
@@ -281,6 +285,7 @@ from flag_gems.ops.pow import (
     pow_tensor_tensor_,
 )
 from flag_gems.ops.prelu import prelu
+from flag_gems.ops.prelu_backward import prelu_backward
 from flag_gems.ops.prod import prod, prod_dim
 from flag_gems.ops.quantile import quantile
 from flag_gems.ops.rad2deg import rad2deg, rad2deg_
@@ -304,7 +309,9 @@ from flag_gems.ops.repeat_interleave import (
     repeat_interleave_tensor,
 )
 from flag_gems.ops.replication_pad1d import replication_pad1d, replication_pad1d_out
+from flag_gems.ops.replication_pad1d_backward import replication_pad1d_backward
 from flag_gems.ops.replication_pad3d import replication_pad3d
+from flag_gems.ops.replication_pad3d_backward import replication_pad3d_backward
 from flag_gems.ops.resolve_conj import resolve_conj
 from flag_gems.ops.resolve_neg import resolve_neg
 from flag_gems.ops.rms_norm import rms_norm, rms_norm_backward, rms_norm_forward
@@ -326,6 +333,7 @@ from flag_gems.ops.select_backward import select_backward
 from flag_gems.ops.select_scatter import select_scatter
 from flag_gems.ops.selu import selu
 from flag_gems.ops.selu_ import selu_
+from flag_gems.ops.selu_backward import selu_backward
 from flag_gems.ops.sgn_ import sgn_
 from flag_gems.ops.sigmoid import sigmoid, sigmoid_, sigmoid_backward
 from flag_gems.ops.signbit import signbit, signbit_out
@@ -348,6 +356,7 @@ from flag_gems.ops.softmax import (
 )
 from flag_gems.ops.softplus import softplus
 from flag_gems.ops.softshrink import softshrink, softshrink_out
+from flag_gems.ops.softshrink_backward import softshrink_backward
 from flag_gems.ops.sort import sort, sort_stable
 from flag_gems.ops.special_i0e import special_i0e, special_i0e_out
 from flag_gems.ops.special_i1 import special_i1, special_i1_out
@@ -495,6 +504,7 @@ __all__ = [
     "ceil_out",
     "celu",
     "celu_",
+    "celu_backward",
     "clamp",
     "clamp_",
     "clamp_max",
@@ -623,6 +633,7 @@ __all__ = [
     "hadamard_transform_40N",
     "hardsigmoid",
     "hardsigmoid_out",
+    "hardsigmoid_backward",
     "hardswish_",
     "histc",
     "hstack",
@@ -684,6 +695,7 @@ __all__ = [
     "logit",
     "logit_",
     "logit_out",
+    "logit_backward",
     "logspace",
     "logsumexp",
     "lt",
@@ -752,6 +764,7 @@ __all__ = [
     "pow_tensor_tensor",
     "pow_tensor_tensor_",
     "prelu",
+    "prelu_backward",
     "prod",
     "prod_dim",
     "quantile",
@@ -780,8 +793,10 @@ __all__ = [
     "repeat_interleave_self_tensor",
     "repeat_interleave_tensor",
     "replication_pad1d",
+    "replication_pad1d_backward",
     "replication_pad1d_out",
     "replication_pad3d",
+    "replication_pad3d_backward",
     "resolve_conj",
     "resolve_neg",
     "rms_norm",
@@ -815,6 +830,7 @@ __all__ = [
     "select_scatter",
     "selu",
     "selu_",
+    "selu_backward",
     "sgn_",
     "sigmoid",
     "sigmoid_",
@@ -841,6 +857,9 @@ __all__ = [
     "softplus",
     "softshrink",
     "softshrink_out",
+    "softshrink_backward",
+    "hardshrink",
+    "hardshrink_backward",
     "sort",
     "sort_stable",
     "special_i0e",
---
+++ tests/test_hardsigmoid_backward.py
@@ -0,0 +1,25 @@
+import pytest
+import torch
+
+import flag_gems
+
+from . import accuracy_utils as utils
+
+
+@pytest.mark.hardsigmoid_backward
+@pytest.mark.parametrize("shape", utils.POINTWISE_SHAPES)
+@pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
+def test_hardsigmoid_backward(shape, dtype):
+    """Test hardsigmoid_backward with various shapes."""
+    x = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    grad_output = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+
+    ref_x = utils.to_reference(x)
+    ref_grad_output = utils.to_reference(grad_output)
+
+    ref_grad_input = torch.ops.aten.hardsigmoid_backward(ref_grad_output, ref_x)
+
+    with flag_gems.use_gems():
+        res_grad_input = torch.ops.aten.hardsigmoid_backward(grad_output, x)
+
+    utils.gems_assert_close(res_grad_input, ref_grad_input, dtype)
