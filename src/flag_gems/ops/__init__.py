dadfa303f222dea16aab30419ded705f64d5750b
XDYuanzhuLee <2680679588@qq.com>
2026-06-03 04:27:12 +0000

    Add __ilshift__ inplace bitwise left shift operator

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -1,3 +1,4 @@
+from flag_gems.ops.__ilshift__ import __ilshift__
 from flag_gems.ops._euclidean_dist import _euclidean_dist
 from flag_gems.ops._functional_sym_constrain_range_for_size import (
     _functional_sym_constrain_range_for_size,
@@ -400,6 +401,7 @@ from flag_gems.ops.zeros import zero_, zeros
 from flag_gems.ops.zeros_like import zeros_like
 
 __all__ = [
+    "__ilshift__",
     "_assert_async",
     "_conv_depthwise2d",
     "_euclidean_dist",
