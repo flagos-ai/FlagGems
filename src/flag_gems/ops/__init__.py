54cab5d21ff03729cc2dbea504b362c7c48e6b97
taooo <gumptao2997@gmail.com>
2026-06-05 04:45:39 +0000

    [KernelGen][Nvidia] Add log_normal_ operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -203,6 +203,7 @@ from flag_gems.ops.log import log
 from flag_gems.ops.log1p import log1p
 from flag_gems.ops.log1p_ import log1p_
 from flag_gems.ops.log10 import log10, log10_, log10_out
+from flag_gems.ops.log_normal_ import log_normal_
 from flag_gems.ops.log_sigmoid import log_sigmoid
 from flag_gems.ops.log_softmax import (
     log_softmax,
@@ -675,6 +676,7 @@ __all__ = [
     "log10_",
     "log10_out",
     "log1p_",
+    "log_normal_",
     "log_sigmoid",
     "log_softmax",
     "log_softmax_backward",
@@ -682,6 +684,7 @@ __all__ = [
     "log_softmax_out",
     "log1p",
     "log1p_",
+    "log_normal_",
     "logaddexp",
     "logaddexp_out",
     "logical_and",
