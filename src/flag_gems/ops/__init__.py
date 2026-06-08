2913a3aa3d9cc703f7c16fd1267bff6aa4cd41bb
XDYuanzhuLee <2680679588@qq.com>
2026-06-08 07:17:10 +0000

    [KernelGen][Nvidia] Add special_expit operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -361,6 +361,7 @@ from flag_gems.ops.softmax import (
 from flag_gems.ops.softplus import softplus
 from flag_gems.ops.softshrink import softshrink, softshrink_out
 from flag_gems.ops.sort import sort, sort_stable
+from flag_gems.ops.special_expit import special_expit
 from flag_gems.ops.special_i0e import special_i0e, special_i0e_out
 from flag_gems.ops.special_i1 import special_i1, special_i1_out
 from flag_gems.ops.split_with_sizes_copy import split_with_sizes_copy
@@ -871,6 +872,8 @@ __all__ = [
     "softshrink_out",
     "sort",
     "sort_stable",
+    "special_expit",
+    "special_expit",
     "special_i0e",
     "special_i0e_out",
     "special_i1",
