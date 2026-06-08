0a568953f52e572ec000e898263b0ec962607dc4
LoserCheems <losercheems@gmail.com>
2026-06-08 17:26:25 +0800

    feat: add _upsample_nearest_exact3d operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -71,6 +71,7 @@ _FULL_CONFIG = (
     ("_upsample_bicubic2d_aa", _upsample_bicubic2d_aa),
     ("_upsample_bicubic2d_aa_backward", _upsample_bicubic2d_aa_backward),
     ("_upsample_nearest_exact1d", _upsample_nearest_exact1d),
+    ("_upsample_nearest_exact3d", _upsample_nearest_exact3d),
     ("_weight_norm_interface", weight_norm_interface),
     ("_weight_norm_interface_backward", weight_norm_interface_backward),
     ("abs", abs),
