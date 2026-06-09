342d85b36fdf4bd246bd72e8cd65276741d55f4b
bwbwzzz <957058626@qq.com>
2026-06-09 02:51:31 +0000

    [KernelGen][Nvidia] Add special_psi operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -363,6 +363,7 @@ from flag_gems.ops.softshrink import softshrink, softshrink_out
 from flag_gems.ops.sort import sort, sort_stable
 from flag_gems.ops.special_i0e import special_i0e, special_i0e_out
 from flag_gems.ops.special_i1 import special_i1, special_i1_out
+from flag_gems.ops.special_psi import special_psi
 from flag_gems.ops.split_with_sizes_copy import split_with_sizes_copy
 from flag_gems.ops.sqrt import sqrt, sqrt_
 from flag_gems.ops.square import square, square_, square_out
@@ -875,6 +876,7 @@ __all__ = [
     "special_i0e_out",
     "special_i1",
     "special_i1_out",
+    "special_psi",
     "split_with_sizes_copy",
     "sqrt",
     "sqrt_",
