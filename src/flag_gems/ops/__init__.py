5f31d3e8fa964859b873bd95bb2f714b4a326680
XDYuanzhuLee <2680679588@qq.com>
2026-06-09 07:22:35 +0000

    [KernelGen][Nvidia] Add lgamma_ operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -199,6 +199,7 @@ from flag_gems.ops.layernorm import layer_norm, layer_norm_backward
 from flag_gems.ops.le import le, le_scalar
 from flag_gems.ops.leaky_relu import leaky_relu, leaky_relu_, leaky_relu_out
 from flag_gems.ops.lerp import lerp_scalar, lerp_scalar_, lerp_tensor, lerp_tensor_
+from flag_gems.ops.lgamma_ import lgamma, lgamma_
 from flag_gems.ops.lift_fresh_copy import lift_fresh_copy, lift_fresh_copy_out
 from flag_gems.ops.linspace import linspace
 from flag_gems.ops.log import log
@@ -678,6 +679,8 @@ __all__ = [
     "lerp_scalar_",
     "lerp_tensor",
     "lerp_tensor_",
+    "lgamma",
+    "lgamma_",
     "lift_fresh_copy",
     "lift_fresh_copy_out",
     "linspace",
