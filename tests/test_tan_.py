a1d52aa4bf74a63bcd5b12d8042be6afeef38d6d
XDYuanzhuLee <2680679588@qq.com>
2026-06-11 07:13:24 +0000

    [KernelGen][Nvidia] Add tan_ operator with Triton kernel

---
+++ tests/test_tan_.py
@@ -0,0 +1,21 @@
+import pytest
+import torch
+
+import flag_gems
+
+from . import accuracy_utils as utils
+
+
+@pytest.mark.tan_
+@pytest.mark.parametrize("shape", utils.POINTWISE_SHAPES)
+@pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
+def test_tan_(shape, dtype):
+    inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    ref_inp = utils.to_reference(inp.clone(), True)
+
+    ref_out = torch.tan_(ref_inp)
+    with flag_gems.use_gems():
+        res_out = torch.tan_(inp)
+
+    utils.gems_assert_close(res_out, ref_out, dtype)
+    utils.gems_assert_close(inp, ref_inp, dtype)
