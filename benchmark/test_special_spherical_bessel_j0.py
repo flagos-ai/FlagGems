75c674f490836b23889795a56afdefa1af725ebf
bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
2026-06-11 12:10:30 +0800

    Add benchmark for special_spherical_bessel_j0_ function
    Signed-off-by: bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>

--- benchmark/test_special_spherical_bessel_j0.py
+++ benchmark/test_special_spherical_bessel_j0.py
@@ -13,3 +13,14 @@ def test_special_spherical_bessel_j0():
         dtypes=[torch.float32],
     )
     bench.run()
+
+
+@pytest.mark.special_spherical_bessel_j0_
+def test_special_spherical_bessel_j0_():
+    bench = base.UnaryPointwiseBenchmark(
+        op_name="special_spherical_bessel_j0_",
+        torch_op=torch.special.spherical_bessel_j0_,
+        # torch.special.spherical_bessel_j0 does not support fp16/bf16
+        dtypes=[torch.float32],
+    )
+    bench.run()
