4a7f002f38900fc0df926fd96ba51b998d952ed0
binliu <87918526+llaboon@users.noreply.github.com>
2026-06-15 18:13:33 +0800

    Update conv3d.py
    Signed-off-by: binliu <87918526+llaboon@users.noreply.github.com>

--- src/flag_gems/runtime/backend/_kunlunxin/ops/conv3d.py
+++ src/flag_gems/runtime/backend/_kunlunxin/ops/conv3d.py
@@ -277,7 +277,6 @@ def conv3d(input, weight, bias=None, stride=1, padding=0, dilation=1, groups=1):
                 dilation_width,
             )
 
-
             input = torch.nn.functional.pad(
                 input,
                 (
