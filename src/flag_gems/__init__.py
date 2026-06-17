3a8cc6e14c5f7213f095e0d6ba9012e90ec84ba7
Dingxingdi <784318034@qq.com>
2026-06-17 04:00:01 +0000

    [KernelGen][Nvidia] Add linalg_ldl_factor_ex operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -354,6 +354,7 @@ _FULL_CONFIG = (
     ("lerp_.Scalar", lerp_scalar_),
     ("lerp_.Tensor", lerp_tensor_),
     ("lift_fresh_copy", lift_fresh_copy),
+    ("linalg_ldl_factor_ex", ldl_factor_ex),
     ("linalg_vector_norm", vector_norm),
     ("linear", linear),
     ("linspace", linspace),
