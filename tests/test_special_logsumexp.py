78479ecf0361c0edb3f90af970d312cb6cae70d3
yangy0906 <yangyang0906c@163.com>
2026-06-18 03:31:10 +0000

    Add dedicated test for special_logsumexp multi-dim reduction branch
    Address reviewer feedback: add test_special_logsumexp_multi_dim that
    covers the len(dim) > 1 path with 3D inputs and multiple dim
    combinations ([0,1], [0,2], [1,2]) across all float dtypes, both
    keepdim modes.

--- tests/test_special_logsumexp.py
+++ tests/test_special_logsumexp.py
@@ -20,3 +20,20 @@ def test_special_logsumexp(shape, dtype, dim, keepdim):
         res_out = torch.special.logsumexp(inp, dim=dim, keepdim=keepdim)
 
     utils.gems_assert_close(res_out, ref_out, dtype)
+
+
+@pytest.mark.special_logsumexp
+@pytest.mark.parametrize("shape", [(16, 64, 128)])
+@pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
+@pytest.mark.parametrize("dims", [[0, 1], [0, 2], [1, 2]])
+@pytest.mark.parametrize("keepdim", [True, False])
+def test_special_logsumexp_multi_dim(shape, dtype, dims, keepdim):
+    """Dedicated test for the multi-dim (len(dim) > 1) branch."""
+    inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    ref_inp = utils.to_reference(inp, True)
+
+    ref_out = torch.special.logsumexp(ref_inp, dim=dims, keepdim=keepdim)
+    with flag_gems.use_gems():
+        res_out = torch.special.logsumexp(inp, dim=dims, keepdim=keepdim)
+
+    utils.gems_assert_close(res_out, ref_out, dtype)
