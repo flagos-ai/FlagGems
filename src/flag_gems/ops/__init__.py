7b26cf40591cf1969897a7740be6d143d920e6b0
XDYuanzhuLee <93123184+XDYuanzhuLee@users.noreply.github.com>
2026-06-19 03:26:38 +0000

    [KernelGen][Nvidia] Fix addmm_ review comments

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -435,7 +435,6 @@ from flag_gems.ops.zeros import zero_, zeros
 from flag_gems.ops.zeros_like import zeros_like
 
 __all__ = [
-    "addmm_",
     "SUPPORTED_FP8_DTYPE",
     "ScaleDotProductAttention",
     "_assert_async",
@@ -468,6 +467,7 @@ __all__ = [
     "addmm_dtype",
     "addmm_dtype_out",
     "addmm_out",
+    "addmm_",
     "addmv",
     "addmv_out",
     "addr",
