92d80d3a803764701730944ff280947151f972ba
XDYuanzhuLee <93123184+XDYuanzhuLee@users.noreply.github.com>
2026-06-19 06:53:39 +0000

    [KernelGen][Nvidia] Fix special_xlogy review comments

--- tests/test_special_xlogy.py
+++ tests/test_special_xlogy.py
@@ -39,6 +39,7 @@ def test_special_xlogy_(shape, dtype):
     utils.gems_assert_close(inp1, ref_inp1, dtype, equal_nan=True)
 
 
+@pytest.mark.special_xlogy
 @pytest.mark.parametrize("shape", utils.POINTWISE_SHAPES)
 @pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
 def test_special_xlogy_tensor_scalar(shape, dtype):
@@ -53,6 +54,7 @@ def test_special_xlogy_tensor_scalar(shape, dtype):
     utils.gems_assert_close(res_out, ref_out, dtype, equal_nan=True)
 
 
+@pytest.mark.special_xlogy
 @pytest.mark.parametrize("shape", utils.POINTWISE_SHAPES)
 @pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
 def test_special_xlogy_scalar_tensor(shape, dtype):
