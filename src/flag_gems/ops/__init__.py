539c2deb78e5bcfbdbbfd4f74b185734a79a1047
Dingxingdi <784318034@qq.com>
2026-06-22 08:16:12 +0000

    [KernelGen][Nvidia] Add linalg_slogdet operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -217,6 +217,7 @@ from flag_gems.ops.le import le, le_scalar
 from flag_gems.ops.leaky_relu import leaky_relu, leaky_relu_, leaky_relu_out
 from flag_gems.ops.lerp import lerp_scalar, lerp_scalar_, lerp_tensor, lerp_tensor_
 from flag_gems.ops.lift_fresh_copy import lift_fresh_copy, lift_fresh_copy_out
+from flag_gems.ops.linalg_slogdet import slogdet, slogdet_
 from flag_gems.ops.linear import linear
 from flag_gems.ops.linspace import linspace
 from flag_gems.ops.log import log
@@ -932,6 +933,8 @@ __all__ = [
     "sinh_",
     "slice_backward",
     "slice_scatter",
+    "slogdet",
+    "slogdet_",
     "smooth_l1_loss",
     "smooth_l1_loss_backward",
     "smooth_l1_loss_out",
