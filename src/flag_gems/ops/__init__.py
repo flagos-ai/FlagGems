a571869d587fd7bed3bdd7855398eed5caf92440
Dingxingdi <784318034@qq.com>
2026-06-23 07:09:09 +0000

    [KernelGen][Nvidia] Add logaddexp2 operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -231,6 +231,7 @@ from flag_gems.ops.log_softmax import (
     log_softmax_out,
 )
 from flag_gems.ops.logaddexp import logaddexp, logaddexp_out
+from flag_gems.ops.logaddexp2 import logaddexp2, logaddexp2_out
 from flag_gems.ops.logical_and import logical_and, logical_and_
 from flag_gems.ops.logical_not import logical_not, logical_not_
 from flag_gems.ops.logical_or import logical_or, logical_or_
@@ -753,6 +754,8 @@ __all__ = [
     "log_softmax_backward_out",
     "log_softmax_out",
     "logaddexp",
+    "logaddexp2",
+    "logaddexp2_out",
     "logaddexp_out",
     "logical_and",
     "logical_and_",
