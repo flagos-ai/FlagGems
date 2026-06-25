822490bbe2dfebc928bc52bca24b148b022fa8fb
bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
2026-06-25 18:41:42 +0800

    Add parameterized test for arctan function
    Signed-off-by: bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>

--- tests/test_arctan_.py
+++ tests/test_arctan_.py
@@ -6,6 +6,21 @@ import flag_gems
 from . import accuracy_utils as utils
 
 
+@pytest.mark.arctan
+@pytest.mark.parametrize("shape", utils.POINTWISE_SHAPES)
+@pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
+def test_arctan(shape, dtype):
+    inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
+    ref_inp = utils.to_reference(inp)
+
+    ref_out = torch.arctan(ref_inp)
+    with flag_gems.use_gems():
+        res_out = torch.arctan(inp)
+
+    ref_out = ref_out.to(res_out.dtype)
+    utils.gems_assert_close(res_out, ref_out, dtype)
+
+
 @pytest.mark.arctan_
 @pytest.mark.parametrize("shape", utils.POINTWISE_SHAPES)
 @pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
