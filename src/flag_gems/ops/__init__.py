a2cb2160f30d0cbc26bee75f6d46258bdfe72c67
LoserCheems <losercheems@gmail.com>
2026-06-26 16:24:12 +0800

    style: fix formatting via pre-commit

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -139,8 +139,8 @@ from flag_gems.ops.dequantize import dequantize
 from flag_gems.ops.diag import diag
 from flag_gems.ops.diag_embed import diag_embed
 from flag_gems.ops.diagonal import diagonal_backward
-from flag_gems.ops.diagonal_scatter import diagonal_scatter
 from flag_gems.ops.diagonal_copy import diagonal_copy
+from flag_gems.ops.diagonal_scatter import diagonal_scatter
 from flag_gems.ops.diff import diff
 from flag_gems.ops.digamma_ import digamma_
 from flag_gems.ops.div import (
@@ -673,7 +673,7 @@ __all__ = [
     "diag",
     "diag_embed",
     "diagonal_backward",
-"diagonal_scatter",
+    "diagonal_scatter",
     "diagonal_copy",
     "diff",
     "digamma_",
