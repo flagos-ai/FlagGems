f276a07cabc3977b1ddde2567e3f113c5b4c4f7e
awayzjj <zjj51234@foxmail.com>
2026-07-01 19:18:01 +0800

    [Iluvatar] Opt tile repeat (#4458)
    * optimize repeat
    
    * optimze tile
    
    ---------
    
    Signed-off-by: awayzjj <zjj51234@foxmail.com>
    Co-authored-by: Qiming Teng <tengqm@outlook.com>

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -2,6 +2,8 @@ from .div import div_mode, div_mode_
 from .hadamard_transform import hadamard_transform
 from .matmul_bf16 import matmul_bf16
 from .matmul_int8 import matmul_int8
+from .repeat import repeat
+from .tile import tile
 from .var import var, var_correction, var_dim
 
 __all__ = [
@@ -10,6 +12,8 @@ __all__ = [
     "hadamard_transform",
     "matmul_bf16",
     "matmul_int8",
+    "repeat",
+    "tile",
     "var",
     "var_correction",
     "var_dim",
