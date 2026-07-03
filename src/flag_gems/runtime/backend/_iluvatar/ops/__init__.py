74909d65b466bc24002c79dbaff22ff8ad29eda2
huatuo.li <huatuoli@126.com>
2026-07-03 16:19:49 +0800

    [Iluvatar] Opt Iluvatar conv_depthwise2d.

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -1,3 +1,4 @@
+from .conv_depthwise2d import _conv_depthwise2d
 from .div import div_mode, div_mode_
 from .hadamard_transform import hadamard_transform
 from .matmul_bf16 import matmul_bf16
@@ -17,4 +18,5 @@ __all__ = [
     "var",
     "var_correction",
     "var_dim",
+    "_conv_depthwise2d",
 ]
