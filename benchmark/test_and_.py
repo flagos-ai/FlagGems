393c9aa7c8076786ca9cba81dec2776f37529452
Dingxingdi <784318034@qq.com>
2026-07-04 12:32:10 +0800

    Update benchmark/test_and_.py
    Co-authored-by: Qiming Teng <tengqm@outlook.com>
    Signed-off-by: Dingxingdi <784318034@qq.com>

--- benchmark/test_and_.py
+++ benchmark/test_and_.py
@@ -6,7 +6,7 @@ from . import base, consts
 @pytest.mark.and_
 def test_and():
     bench = base.BinaryPointwiseBenchmark(
-        op_name="and_",
+        op_name="and",
         torch_op=lambda a, b: a.__and__(b),
         dtypes=consts.INT_DTYPES + consts.BOOL_DTYPES,
     )
