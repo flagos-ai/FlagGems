dd88a82d7062e2a4cc1671167d9bc24aa2b6f4a1
0x45f <wangzhen@baai.ac.cn>
2026-07-06 10:18:16 +0000

    Refine code

--- src/flag_gems/ops/randn.py
+++ src/flag_gems/ops/randn.py
@@ -58,18 +58,6 @@ device_ = device
 logger = logging.getLogger(__name__)
 
 
-# # @triton.heuristics(runtime.get_heuristic_config("randn"))
-# configs = [
-#     triton.Config({"BLOCK": 256}, num_warps=8, num_stages=2),
-#     triton.Config({"BLOCK": 512}, num_warps=4, num_stages=2),
-#     triton.Config({"BLOCK": 512}, num_warps=8, num_stages=3),
-#     triton.Config({"BLOCK": 1024}, num_warps=4, num_stages=2),
-#     triton.Config({"BLOCK": 1024}, num_warps=8, num_stages=3),
-#     triton.Config({"BLOCK": 1024}, num_warps=8, num_stages=4),
-# ]
-
-
-# @triton.autotune(configs=configs, key=["N"])
 @triton.jit(do_not_specialize=["philox_seed", "philox_offset"])
 def randn_kernel(
     out_ptr,
