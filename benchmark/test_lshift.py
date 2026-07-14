cc505e5898676292708330f55edaeabcbbbe1583
bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
2026-07-14 16:51:13 +0800

    Apply suggestion from @bwbwzzz
    Signed-off-by: bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>

--- benchmark/test_lshift.py
+++ benchmark/test_lshift.py
@@ -7,7 +7,7 @@ from . import base, consts
 @pytest.mark.lshift
 def test_lshift():
     bench = base.BinaryPointwiseBenchmark(
-        op_name="lshift__",
+        op_name="lshift",
         torch_op=torch.bitwise_left_shift,
         dtypes=consts.INT_DTYPES,
     )
