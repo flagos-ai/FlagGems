f0af03917b944977b4e6c8ab85b8deef642d7d65
Dongxu-H <dxhan@baai.ac.cn>
2026-07-16 12:50:27 +0800

    Update src/flag_gems/__init__.py
    Co-authored-by: wanghairui <wanghairui0927@gmail.com>
    Signed-off-by: Dongxu-H <dxhan@baai.ac.cn>

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -630,7 +630,7 @@ _FULL_CONFIG = (
     ("sort.stable", sort_stable),
     ("special.gammainc", special_gammainc),
     ("special.log_softmax", special_log_softmax),
-    ("special.xlog1py", special_xlog1py),
+    ("special_xlog1py", special_xlog1py),
     ("special_chebyshev_polynomial_v", special_chebyshev_polynomial_v),
     ("special_hermite_polynomial_h", special_hermite_polynomial_h),
     ("special_i0e", special_i0e),
