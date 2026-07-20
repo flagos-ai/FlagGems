f72a1009a4083b8bd9b494b2ae0a8a239df268e4
kkkwb <1115095230@qq.com>
2026-07-20 14:46:37 +0800

    Fix(special_sinc):Fix special_sinc registration and drop invalid in-place variant

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -440,7 +440,7 @@ from flag_gems.ops.special_shifted_chebyshev_polynomial_u import (
     special_shifted_chebyshev_polynomial_u,
     special_shifted_chebyshev_polynomial_u_,
 )
-from flag_gems.ops.special_sinc import special_sinc, special_sinc_
+from flag_gems.ops.special_sinc import special_sinc
 from flag_gems.ops.split_with_sizes_copy import split_with_sizes_copy
 from flag_gems.ops.sqrt import sqrt, sqrt_
 from flag_gems.ops.square import square, square_, square_out
@@ -1031,7 +1031,6 @@ __all__ = [
     "special_shifted_chebyshev_polynomial_u",
     "special_shifted_chebyshev_polynomial_u_",
     "special_sinc",
-    "special_sinc_",
     "split_with_sizes_copy",
     "sqrt",
     "sqrt_",
--- tests/test_special_sinc.py
+++ tests/test_special_sinc.py
@@ -18,18 +18,3 @@ def test_special_sinc(shape, dtype):
         res_out = torch.special.sinc(inp)
 
     utils.gems_assert_close(res_out, ref_out, dtype)
-
-
-@pytest.mark.special_sinc_
-@pytest.mark.parametrize("shape", utils.POINTWISE_SHAPES)
-@pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES)
-def test_special_sinc_(shape, dtype):
-    inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
-    ref_inp = utils.to_reference(inp.clone())
-
-    ref_out = ref_inp.sinc_()
-    with flag_gems.use_gems():
-        res_out = inp.sinc_()
-
-    utils.gems_assert_close(res_out, ref_out, dtype)
-    utils.gems_assert_close(inp, ref_inp, dtype)
