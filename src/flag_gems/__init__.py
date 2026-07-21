39f2ea079690af3b069f504495c9ae2d2049683b
bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
2026-07-21 11:15:55 +0800

    [KernelGen][Nvidia] Add special_modified_bessel_k0 operator with Triton kernel (#3699)
    * [KernelGen][Nvidia] Add special_modified_bessel_k0 operator with Triton kernel
    
    * Add special_modified_bessel_k0_out Triton kernel
    
    Added Triton kernel implementation for special_modified_bessel_k0.out.
    
    Signed-off-by: bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
    
    * Add stable stage for special_log_softmax operator
    
    Signed-off-by: Dongxu-H <dxhan@baai.ac.cn>
    
    ---------
    
    Signed-off-by: bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
    Signed-off-by: Dongxu-H <dxhan@baai.ac.cn>
    Co-authored-by: XDYuanzhuLee <2680679588@qq.com>
    Co-authored-by: Dongxu-H <dxhan@baai.ac.cn>

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -745,6 +745,8 @@ _FULL_CONFIG = (
     ("special_log1p.out", special_log1p_out),
     ("special_logsumexp", special_logsumexp),
     ("special_log_softmax", special_log_softmax),
+    ("special_modified_bessel_k0", special_modified_bessel_k0),
+    ("special_modified_bessel_k0.out", special_modified_bessel_k0_out),
     ("special_shifted_chebyshev_polynomial_u", special_shifted_chebyshev_polynomial_u),
     (
         "special_shifted_chebyshev_polynomial_u_",
