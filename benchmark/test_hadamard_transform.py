290f053c34d2cf357bec77dd9b71120e43a13368
dawnyang050-debug <dawnyang050@gmail.com>
2026-07-22 15:11:33 +0800

    修复问题

--- benchmark/test_hadamard_transform.py
+++ benchmark/test_hadamard_transform.py
@@ -1,4 +1,3 @@
-
 # Copyright 2026 FlagOS Contributors
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
@@ -12,7 +11,9 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 import math
+
 import pytest
 import torch
 import torch.nn.functional as F
