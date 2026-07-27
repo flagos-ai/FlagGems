028a86691a99b7bfb9246c49eb748c0742ab0ad9
Dongxu-H <dxhan@baai.ac.cn>
2026-07-27 16:55:14 +0800

    Reorder import statements in test_mish.py
    Signed-off-by: Dongxu-H <dxhan@baai.ac.cn>

--- benchmark/test_mish.py
+++ benchmark/test_mish.py
@@ -12,10 +12,11 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from typing import Generator
+
 import pytest
 import torch
 
-from typing import Generator
 from . import base, consts, utils
 
 
