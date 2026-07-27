8a66f01d08dac0b7743751c29b2bac7facaa9d6d
LinuxKiller <wangzixu13145687@gmail.com>
2026-07-27 17:05:04 +0800

    fix(metax): fix index reduce prod for unique indices

--- src/flag_gems/runtime/backend/_metax/ops/__init__.py
+++ src/flag_gems/runtime/backend/_metax/ops/__init__.py
@@ -11,6 +11,7 @@ from .groupnorm import group_norm
 from .hadamard_transform import hadamard_transform
 from .index import index
 from .index_put import index_put, index_put_
+from .index_reduce import index_reduce_
 from .index_select import index_select
 from .isin import isin
 from .layernorm import layer_norm, layer_norm_backward
@@ -67,6 +68,7 @@ __all__ = [
     "index",
     "index_put",
     "index_put_",
+    "index_reduce_",
     "index_select",
     "isin",
     "layer_norm",
