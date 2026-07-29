df156af4894388c9a134b2a4f27103996fe01077
LoserCheems <losercheems@gmail.com>
2026-07-29 17:22:32 +0800

    [KernelGen][Nvidia] Add alias operator

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -192,6 +192,7 @@ _FULL_CONFIG = (
     ("addmv.out", addmv_out),
     ("addr", addr),
     ("affine_grid_generator", affine_grid_generator),
+    ("alias", alias),
     ("alias_copy", alias_copy),
     ("all", all),
     ("all.dim", all_dim),
