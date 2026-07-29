7448f90ec7ba7d23f6747387a23f5585ea60045f
LinuxKiller <wangzixu13145687@gmail.com>
2026-07-29 17:11:21 +0800

    fix(iluvatar): fix and optimize nanmedian

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -22,6 +22,7 @@ from .linear import linear
 from .matmul_bf16 import matmul_bf16
 from .matmul_int8 import matmul_int8
 from .mm import mm, mm_out
+from .nanmedian import nanmedian, nanmedian_dim, nanmedian_dim_values, nanmedian_out
 from .repeat import repeat
 from .scatter_add import scatter_add_
 from .special_modified_bessel_k1 import (
@@ -57,4 +58,8 @@ __all__ = [
     "var_dim",
     "mm",
     "mm_out",
+    "nanmedian",
+    "nanmedian_dim",
+    "nanmedian_dim_values",
+    "nanmedian_out",
 ]
