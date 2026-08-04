ff614f63dd332686d3222af2e5c7bfa0c21a67c7
LoserCheems <losercheems@gmail.com>
2026-08-04 14:37:26 +0800

    [KernelGen][Nvidia] Add linalg_householder_product operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -113,6 +113,7 @@ _FULL_CONFIG = (
     ("_jagged_to_padded_dense_forward", _jagged_to_padded_dense_forward),
     ("_linalg_eigvals", _linalg_eigvals),
     ("binary_cross_entropy_backward", binary_cross_entropy_backward),
+    ("linalg_householder_product", linalg_householder_product),
     ("linalg_svdvals", linalg_svdvals),
     ("_log_softmax", log_softmax),
     ("_log_softmax.out", log_softmax_out),
