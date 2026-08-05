35d461957a58ac3ce15618ceb1bbe5881f5333a2
taooo <gumptao2997@gmail.com>
2026-08-05 20:12:36 +0800

    Add _has_compatible_shallow_copy_type benchmark

---
+++ benchmark/test_has_compatible_shallow_copy_type.py
@@ -0,0 +1,36 @@
+# Copyright 2026 FlagOS Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+import torch
+
+from . import base, consts
+
+
+@pytest.mark.has_compatible_shallow_copy_type
+def test_has_compatible_shallow_copy_type():
+    def has_compatible_shallow_copy_type_input_fn(shape, dtype, device):
+        # Metadata-only check between two tensors of the same layout.
+        inp1 = torch.randn(shape, dtype=dtype, device=device)
+        inp2 = torch.randn(shape, dtype=dtype, device=device)
+        yield inp1, inp2
+
+    bench = base.GenericBenchmark(
+        input_fn=has_compatible_shallow_copy_type_input_fn,
+        op_name="has_compatible_shallow_copy_type",
+        torch_op=torch._has_compatible_shallow_copy_type,
+        dtypes=consts.FLOAT_DTYPES,
+    )
+    bench.set_gems(lambda x, y: torch._has_compatible_shallow_copy_type(x, y))
+    bench.run()
