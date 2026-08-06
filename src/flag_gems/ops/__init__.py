9d260142a58281c51e8659a1a08f028685a11091
LoserCheems <losercheems@gmail.com>
2026-08-06 16:17:38 +0800

    [KernelGen][Nvidia] Add linalg_inv_ex operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -361,6 +361,7 @@ from flag_gems.ops.lgamma_ import lgamma, lgamma_
 from flag_gems.ops.lift import lift, lift_out
 from flag_gems.ops.lift_fresh_copy import lift_fresh_copy, lift_fresh_copy_out
 from flag_gems.ops.linalg_cholesky import linalg_cholesky
+from flag_gems.ops.linalg_inv_ex import linalg_inv_ex
 from flag_gems.ops.linalg_ldl_factor import ldl_factor
 from flag_gems.ops.linalg_ldl_solve import linalg_ldl_solve
 from flag_gems.ops.linalg_lu_factor import linalg_lu_factor, linalg_lu_factor_out
@@ -1125,6 +1126,7 @@ __all__ = [
     "lift_fresh_copy_out",
     "lift_out",
     "linalg_cholesky",
+    "linalg_inv_ex",
     "linalg_ldl_solve",
     "linalg_lu_factor",
     "linalg_lu_factor_out",
