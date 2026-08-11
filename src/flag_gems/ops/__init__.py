0dfad402cee282f7f69fdf8c2d56fd1d22ea0383
Coye_Chen <52786734+CoyeCHEN@users.noreply.github.com>
2026-08-11 18:18:52 +0800

    [KernelGen][Nvidia] Add special_shifted_chebyshev_polynomial_t operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -671,6 +671,10 @@ from flag_gems.ops.special_scaled_modified_bessel_k1 import (
     special_scaled_modified_bessel_k1,
     special_scaled_modified_bessel_k1_out,
 )
+from flag_gems.ops.special_shifted_chebyshev_polynomial_t import (
+    special_shifted_chebyshev_polynomial_t,
+    special_shifted_chebyshev_polynomial_t_,
+)
 from flag_gems.ops.special_shifted_chebyshev_polynomial_u import (
     special_shifted_chebyshev_polynomial_u,
     special_shifted_chebyshev_polynomial_u_,
@@ -1532,6 +1536,8 @@ __all__ = [
     "special_round_out",
     "special_scaled_modified_bessel_k1",
     "special_scaled_modified_bessel_k1_out",
+    "special_shifted_chebyshev_polynomial_t",
+    "special_shifted_chebyshev_polynomial_t_",
     "special_shifted_chebyshev_polynomial_u",
     "special_shifted_chebyshev_polynomial_u_",
     "special_shifted_chebyshev_polynomial_v",
