3df4c2ceb7994137ba2ad2e5084521a9e4aa23d2
xuanzhengdu-eng <xuanzhengdu@gmail.com>
2026-08-13 22:26:34 +0800

    Add fake_quantize_per_tensor_affine operator
    Signed-off-by: xuanzhengdu-eng <xuanzhengdu@gmail.com>

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -465,6 +465,11 @@ _FULL_CONFIG = (
     ("exponential_", exponential_),
     ("eye", eye),
     ("eye.m", eye_m),
+    ("fake_quantize_per_tensor_affine", fake_quantize_per_tensor_affine),
+    (
+        "fake_quantize_per_tensor_affine.tensor_qparams",
+        fake_quantize_per_tensor_affine,
+    ),
     ("feature_dropout", feature_dropout),
     ("feature_dropout_", feature_dropout_),
     ("fill.Scalar", fill_scalar),
