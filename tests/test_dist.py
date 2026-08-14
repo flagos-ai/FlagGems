6917430a4e6e698bf4d86c7412f6259d26cb9195
chenjinhui <101251828+Keriza@users.noreply.github.com>
2026-08-14 12:32:04 +0800

    Update tests/test_dist.py
    Co-authored-by: wanghairui <wanghairui0927@gmail.com>
    Signed-off-by: chenjinhui <101251828+Keriza@users.noreply.github.com>

--- tests/test_dist.py
+++ tests/test_dist.py
@@ -22,7 +22,7 @@ import flag_gems
     ],
 )
 def test_dist(shape, p, dtype):
-    x = torch.randn(shape, device="cuda", dtype=dtype)
+    x = torch.randn(shape, device=flag_gems.device, dtype=dtype)
     y = torch.randn(shape, device="cuda", dtype=dtype)
 
     with flag_gems.use_gems():
