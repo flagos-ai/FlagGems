5a1349b15a971e0e4c833cfd4bd69340339ead40
Keriza <963297197@qq.com>
2026-08-14 07:46:48 +0000

    Update dist test

--- tests/test_dist.py
+++ tests/test_dist.py
@@ -3,6 +3,8 @@ import torch
 
 import flag_gems
 
+from . import accuracy_utils as utils
+
 
 @pytest.mark.parametrize("p", [0, 1, 2, 3, float("inf"), -float("inf")])
 @pytest.mark.parametrize(
@@ -24,30 +26,26 @@ import flag_gems
 def test_dist(shape, p, dtype):
     x = torch.randn(shape, device=flag_gems.device, dtype=dtype)
     y = torch.randn(shape, device=flag_gems.device, dtype=dtype)
-ref_x = utils.to_reference(x)
-ref_y = utils.to_reference(y)
-ref_p = utils.to_reference(p)
-ref = torch.dist(ref_x, ref_y, ref_p)
+
+    ref_x = utils.to_reference(x)
+    ref_y = utils.to_reference(y)
+    ref_out = torch.dist(ref_x, ref_y, p)
+
     with flag_gems.use_gems():
         out = torch.dist(x, y, p)
 
-
-    if dtype == torch.bfloat16:
-        rtol = 1.6e-2
-    else:
-        rtol = 1e-3
-
-utils.gems_assert_close(res_out, ref_out, dtype)
+    utils.gems_assert_close(out, ref_out, dtype)
 
 
 def test_dist_empty():
-    x = torch.empty(0, device="cuda")
-    y = torch.empty(0, device="cuda")
-ref_x = utils.to_reference(x)
-ref_y = utils.to_reference(y)
-ref_out = torch.dist(ref_x, ref_y)
+    x = torch.empty(0, device=flag_gems.device)
+    y = torch.empty(0, device=flag_gems.device)
+
+    ref_x = utils.to_reference(x)
+    ref_y = utils.to_reference(y)
+    ref_out = torch.dist(ref_x, ref_y)
+
     with flag_gems.use_gems():
         out = torch.dist(x, y)
 
-
-    utils.gems_assert_close(res_out, ref_out, dtype)
+    utils.gems_assert_close(out, ref_out, x.dtype)
