87397122b39ddc137d26c58a31ffb2f272328c87
LoserCheems <losercheems@gmail.com>
2026-08-14 14:29:05 +0800

    [KernelGen][Nvidia] Add linalg_solve_ex operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -583,6 +583,7 @@ from flag_gems.ops.linalg_matrix_sqrth import (
 from flag_gems.ops.linalg_norm import linalg_norm
 from flag_gems.ops.linalg_qr import linalg_qr, linalg_qr_out
 from flag_gems.ops.linalg_slogdet import linalg_slogdet
+from flag_gems.ops.linalg_solve_ex import linalg_solve_ex
 from flag_gems.ops.linalg_solve_triangular import (
     linalg_solve_triangular,
     linalg_solve_triangular_out,
@@ -1619,6 +1620,7 @@ __all__ = [
     "linalg_qr",
     "linalg_qr_out",
     "linalg_slogdet",
+    "linalg_solve_ex",
     "linalg_solve_triangular",
     "linalg_solve_triangular_out",
     "linalg_svd",
