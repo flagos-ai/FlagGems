07f5b5bc09118a8d0ba37be7713773502727e901
chx7514 <hxchen7514@gmail.com>
2026-08-15 10:14:35 +0000

    Add fill_mem_eff_dropout_mask_ operator implementation, tests and benchmark

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -37,6 +37,7 @@ from flag_gems.ops._embedding_bag_per_sample_weights_backward import (
     _embedding_bag_per_sample_weights_backward,
 )
 from flag_gems.ops._euclidean_dist import _euclidean_dist
+from flag_gems.ops._fill_mem_eff_dropout_mask_ import _fill_mem_eff_dropout_mask_
 from flag_gems.ops._flash_attention_forward import _flash_attention_forward
 from flag_gems.ops._functional_sym_constrain_range import (
     _functional_sym_constrain_range,
@@ -826,6 +827,7 @@ __all__ = [
     "_embedding_bag_dense_backward",
     "_embedding_bag_per_sample_weights_backward",
     "_euclidean_dist",
+    "_fill_mem_eff_dropout_mask_",
     "_flash_attention_forward",
     "_functional_sym_constrain_range",
     "_functional_sym_constrain_range_for_size",
