74c80fd970b85fd9d9c5728ddf99a7c55eb470a2
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-08-16 08:01:40 +0000

    [KernelGen][Nvidia] Add _aminmax operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -24,6 +24,7 @@ from flag_gems.ops.__xor__ import (  # noqa: F401
 )
 from flag_gems.ops._adaptive_avg_pool2d_backward import _adaptive_avg_pool2d_backward
 from flag_gems.ops._add_relu import _add_relu
+from flag_gems.ops._aminmax import _aminmax, _aminmax_out
 from flag_gems.ops._amp_foreach_non_finite_check_and_unscale_ import (
     _amp_foreach_non_finite_check_and_unscale_,
 )
@@ -812,6 +813,8 @@ __all__ = [
     "__lshift__",
     "_adaptive_avg_pool2d_backward",
     "_add_relu",
+    "_aminmax",
+    "_aminmax_out",
     "_amp_foreach_non_finite_check_and_unscale_",
     "_assert_async",
     "_batch_norm_no_update",
