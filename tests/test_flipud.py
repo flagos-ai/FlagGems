88cc5934ace05fd3d0453b70be572e7592684e4a
xuanzhengdu-eng <xuanzhengdu@gmail.com>
2026-08-16 23:04:38 +0800

    Fix flipud CPU-reference test
    Signed-off-by: xuanzhengdu-eng <xuanzhengdu@gmail.com>

--- tests/test_flipud.py
+++ tests/test_flipud.py
@@ -29,7 +29,7 @@ from . import accuracy_utils as utils
 def test_accuracy_flipud(shape, dtype):
     inp = torch.randn(shape, dtype=dtype, device=flag_gems.device)
     ref_inp = utils.to_reference(inp, False)
-    original = inp.clone()
+    original = utils.to_reference(inp.clone(), False)
 
     expected = torch.flipud(ref_inp)
     with flag_gems.use_gems():
