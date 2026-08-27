ca9612be819ad646b4346429cf52aff835d78978
xuanzhengdu-eng <xuanzhengdu@gmail.com>
2026-08-27 19:13:38 +0800

    [KernelGen][Nvidia] Add _thnn_differentiable_lstm_cell_backward operator with Triton kernel
    Signed-off-by: xuanzhengdu-eng <xuanzhengdu@gmail.com>

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -125,6 +125,9 @@ from flag_gems.ops._scaled_dot_product_fused_attention_overrideable import (
     _scaled_dot_product_fused_attention_overrideable,
 )
 from flag_gems.ops._sparse_semi_structured_mm import _sparse_semi_structured_mm
+from flag_gems.ops._thnn_differentiable_lstm_cell_backward import (
+    _thnn_differentiable_lstm_cell_backward,
+)
 from flag_gems.ops._thnn_fused_lstm_cell import _thnn_fused_lstm_cell
 from flag_gems.ops._thnn_fused_lstm_cell_backward_impl import (
     _thnn_fused_lstm_cell_backward_impl,
@@ -1008,6 +1011,7 @@ __all__ = [
     "_segment_reduce_backward",
     "_segment_reduce_backward_out",
     "_sparse_semi_structured_mm",
+    "_thnn_differentiable_lstm_cell_backward",
     "_thnn_fused_lstm_cell",
     "_thnn_fused_lstm_cell_backward_impl",
     "_unique2",
