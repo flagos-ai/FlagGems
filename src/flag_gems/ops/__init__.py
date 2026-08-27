e6d3ae0134474616cc50dacae4e3a731fb0dc132
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-08-27 09:00:30 +0000

    [KernelGen][Nvidia] Add _fused_sdp_choice operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -78,6 +78,7 @@ from flag_gems.ops._fused_rms_norm import (
     _fused_rms_norm_backward,
     _fused_rms_norm_forward,
 )
+from flag_gems.ops._fused_sdp_choice import _fused_sdp_choice
 from flag_gems.ops._has_compatible_shallow_copy_type import (
     _has_compatible_shallow_copy_type,
 )
@@ -1004,6 +1005,7 @@ __all__ = [
     "_fused_rms_norm",
     "_fused_rms_norm_backward",
     "_fused_rms_norm_forward",
+    "_fused_sdp_choice",
     "_has_compatible_shallow_copy_type",
     "_index_put_impl_",
     "_is_all_true",
