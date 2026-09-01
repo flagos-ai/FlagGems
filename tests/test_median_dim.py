5512f3d890fb5f352d05d116120af32be3ef559a
huangyiqun <huangyiqun@mail.bnu.edu.cn>
2026-09-01 05:37:10 +0000

    test: register median_dim test coverage

---
+++ tests/test_median_dim.py
@@ -0,0 +1,38 @@
+# Copyright 2026 FlagOS Contributors
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import pytest
+import torch
+
+import flag_gems
+
+from . import accuracy_utils as utils
+
+
+@pytest.mark.median_dim
+@pytest.mark.parametrize("dim", [0, -1])
+@pytest.mark.parametrize("keepdim", [False, True])
+@pytest.mark.parametrize("dtype", utils.FLOAT_DTYPES + utils.ALL_INT_DTYPES)
+def test_median_dim(dim, keepdim, dtype):
+    shape = (3, 17)
+    inp = torch.arange(
+        torch.Size(shape).numel(), device=flag_gems.device, dtype=torch.int64
+    )
+    inp = inp.reshape(shape).to(dtype)
+    ref = torch.median(utils.to_reference(inp), dim=dim, keepdim=keepdim)
+
+    result = flag_gems.median_dim(inp, dim=dim, keepdim=keepdim)
+
+    utils.gems_assert_equal(result.values, ref.values)
+    utils.gems_assert_equal(result.indices, ref.indices)
