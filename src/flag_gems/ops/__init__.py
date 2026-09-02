bac6d01efafb65a38ae643b21713e9fa8abee9d6
littlekid-zxt <291586685@qq.com>
2026-09-02 06:46:55 +0000

    [KMCompiler][NVIDIA][Ascend] add linalg_matrix_exp operator with Triton kernel

--- src/flag_gems/ops/__init__.py
+++ src/flag_gems/ops/__init__.py
@@ -534,6 +534,7 @@ from flag_gems.ops.linalg_lu_factor_ex import (
     linalg_lu_factor_ex,
     linalg_lu_factor_ex_out,
 )
+from flag_gems.ops.linalg_matrix_exp import linalg_matrix_exp, linalg_matrix_exp_out
 from flag_gems.ops.linalg_matrix_norm import linalg_matrix_norm
 from flag_gems.ops.linalg_matrix_rank import (
     linalg_matrix_rank,
@@ -1539,6 +1540,8 @@ __all__ = [
     "linalg_lu_factor_ex_out",
     "linalg_lu_factor_out",
     "linalg_lu_out",
+    "linalg_matrix_exp",
+    "linalg_matrix_exp_out",
     "linalg_matrix_norm",
     "linalg_matrix_rank",
     "linalg_matrix_rank_out",
