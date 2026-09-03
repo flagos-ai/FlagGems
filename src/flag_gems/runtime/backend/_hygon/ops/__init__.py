4e31070f137ea60b3ed05e2d82be35a60a45b8fa
wuhansky <han.wu@compiler-dev.com>
2026-09-03 09:03:11 +0000

    [KMCompiler][Hygon][Iluvatar]Add linalg_matrix_power for triton

--- src/flag_gems/runtime/backend/_hygon/ops/__init__.py
+++ src/flag_gems/runtime/backend/_hygon/ops/__init__.py
@@ -63,6 +63,7 @@ from .isin import isin
 from .lcm import lcm, lcm_
 from .linalg_ldl_factor import ldl_factor
 from .linalg_matrix_norm import linalg_matrix_norm
+from .linalg_matrix_power import linalg_matrix_power, linalg_matrix_power_out
 from .linalg_solve_triangular import (
     linalg_solve_triangular,
     linalg_solve_triangular_out,
@@ -171,6 +172,8 @@ __all__ = [
     "lcm_",
     "ldl_factor",
     "linalg_matrix_norm",
+    "linalg_matrix_power",
+    "linalg_matrix_power_out",
     "linalg_solve_triangular",
     "linalg_solve_triangular_out",
     "log_normal_",
