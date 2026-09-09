e6e465f0af756e1d571cd75925062b854c4f69e5
xuanzhengdu-eng <xuanzhengdu@gmail.com>
2026-09-09 10:45:16 +0800

    [KernelGen][Iluvatar] Add linalg_ldl_factor_ex Iluvatar specialized operator (#359)
    Co-authored-by: xuejing.li <xjli@baai.ac.cn>
    (cherry picked from commit a7979e7e8ab582894fe549ebbd6904a6bffd8644)

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -34,6 +34,7 @@ from .hadamard_transform import hadamard_transform
 from .histc import histc
 from .index_select_backward import index_select_backward
 from .linalg_cholesky import linalg_cholesky
+from .linalg_ldl_factor_ex import ldl_factor_ex
 from .linalg_lstsq import linalg_lstsq
 from .linalg_matrix_norm import linalg_matrix_norm, linalg_matrix_norm_out
 from .linalg_matrix_power import linalg_matrix_power, linalg_matrix_power_out
@@ -157,4 +158,5 @@ __all__ = [
     "var",
     "var_correction",
     "var_dim",
+    "ldl_factor_ex",
 ]
