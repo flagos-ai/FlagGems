0864e0ff8f5b504447d3ce6cb9268ab4df99aba8
xuanzhengdu-eng <xuanzhengdu@gmail.com>
2026-09-09 11:16:25 +0800

    [KernelGen][Iluvatar] Add special_round_out Iluvatar specialized operator (#383)
    Signed-off-by: xuejing.li <xjli@baai.ac.cn>
    Co-authored-by: xuejing.li <xjli@baai.ac.cn>
    (cherry picked from commit f3d35729e76ed900e04e2beecb848c70c9c038cd)

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -75,6 +75,7 @@ from .special_modified_bessel_k1 import (
     special_modified_bessel_k1,
     special_modified_bessel_k1_out,
 )
+from .special_round_out import special_round_out
 from .special_shifted_chebyshev_polynomial_w import (
     special_shifted_chebyshev_polynomial_w,
 )
@@ -153,6 +154,7 @@ __all__ = [
     "special_hermite_polynomial_h",
     "special_modified_bessel_k1",
     "special_modified_bessel_k1_out",
+    "special_round_out",
     "special_shifted_chebyshev_polynomial_w",
     "tile",
     "var",
