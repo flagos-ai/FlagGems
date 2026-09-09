bc202c60d6c38b7458afce8efdaf3e5807e55e84
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-09-09 10:49:08 +0800

    [KernelGen][MThreads] Add gcd_ Moore Threads specialized operator (#433)
    (cherry picked from commit 50ea375d6c63d7b02df5ef327862aa5ceb077319)

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -49,6 +49,7 @@ from .feature_dropout import feature_dropout_
 from .flip import flip
 from .fmod_ import fmod_, fmod_scalar_, fmod_tensor_
 from .gather import gather, gather_backward
+from .gcd_ import gcd_
 from .histc import histc
 from .im2col import im2col
 from .index_add import index_add, index_add_
@@ -161,6 +162,7 @@ __all__ = [
     "fmod_tensor_",
     "gather",
     "gather_backward",
+    "gcd_",
     "histc",
     "im2col",
     "index_add",
