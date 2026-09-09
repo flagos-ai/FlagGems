d786551d8a5d52b309a494db089b8250171bcf5c
xuanzhengdu-eng <xuanzhengdu@gmail.com>
2026-09-09 15:07:36 +0800

    [KernelGen][Iluvatar] Add max_pool3d_with_indices_backward Iluvatar specialized operator (#370)
    Co-authored-by: xuejing.li <xjli@baai.ac.cn>
    Co-authored-by: Dongxu-H <dxhan@baai.ac.cn>
    (cherry picked from commit a014fb5850f6d5e53b59c9356b694c45564ccbc9)

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -55,6 +55,7 @@ from .log_normal import log_normal
 from .log_normal_ import log_normal_
 from .matmul_bf16 import matmul_bf16
 from .matmul_int8 import matmul_int8
+from .max_pool3d_with_indices_backward import max_pool3d_with_indices_backward
 from .median import median
 from .mm import mm, mm_out
 from .mvlgamma import run as mvlgamma
@@ -145,6 +146,7 @@ __all__ = [
     "log_normal_",
     "matmul_bf16",
     "matmul_int8",
+    "max_pool3d_with_indices_backward",
     "median",
     "mm",
     "mm_out",
