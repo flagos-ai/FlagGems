6cd93941e2c06b798ae63e0973c5b56185132177
xuanzhengdu-eng <xuanzhengdu@gmail.com>
2026-09-09 15:07:08 +0800

    [KernelGen][Iluvatar] Add special_legendre_polynomial_p Iluvatar specialized operator (#379)
    Co-authored-by: xuejing.li <xjli@baai.ac.cn>
    Co-authored-by: Dongxu-H <dxhan@baai.ac.cn>
    (cherry picked from commit c098ba3710274aac1a28b303e5d4f7a95c7037a1)

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -78,7 +78,11 @@ from .special_chebyshev_polynomial_w import (
     special_chebyshev_polynomial_w_out,
 )
 from .special_gammainc import special_gammainc
-from .special_hermite_polynomial_h import special_hermite_polynomial_h
+from .special_hermite_polynomial_h import (
+    special_hermite_polynomial_h,
+    special_hermite_polynomial_h_tensor_tensor,
+)
+from .special_legendre_polynomial_p import special_legendre_polynomial_p
 from .special_modified_bessel_k1 import (
     special_modified_bessel_k1,
     special_modified_bessel_k1_out,
@@ -169,6 +173,8 @@ __all__ = [
     "special_chebyshev_polynomial_w_out",
     "special_gammainc",
     "special_hermite_polynomial_h",
+    "special_hermite_polynomial_h_tensor_tensor",
+    "special_legendre_polynomial_p",
     "special_modified_bessel_k1",
     "special_modified_bessel_k1_out",
     "special_multigammaln",
