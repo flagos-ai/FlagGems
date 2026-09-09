43c228f560614387b36862731ab81064b903caa9
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-09-09 15:46:55 +0800

    [KernelGen][MThreads] Add _thnn_fused_lstm_cell_backward_impl Moore Threads specialized operator (#600)
    Co-authored-by: Dongxu-H <dxhan@baai.ac.cn>
    (cherry picked from commit 0d6390803f4537cf3113b2d5f576275ed966996d)

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -15,6 +15,7 @@
 from torch_musa import current_device, get_device_capability
 
 from ._conj import _conj
+from ._thnn_fused_lstm_cell_backward_impl import _thnn_fused_lstm_cell_backward_impl
 from ._upsample_bilinear2d_aa import _upsample_bilinear2d_aa
 from ._upsample_nearest_exact2d_backward import _upsample_nearest_exact2d_backward
 from .adaptive_avg_pool2d_backward import _adaptive_avg_pool2d_backward
@@ -301,6 +302,7 @@ if get_device_capability(current_device())[0] >= 3:
     "_upsample_nearest_exact2d_backward",
     "_upsample_bilinear2d_aa",
     "ldl_factor_ex",
+    "_thnn_fused_lstm_cell_backward_impl",
         ]
     )
 
