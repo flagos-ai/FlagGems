4d093ab21e45a0c57377cbcdda3ecdac2a6c6bf7
xuanzhengdu-eng <xuanzhengdu@gmail.com>
2026-09-09 10:45:26 +0800

    [KernelGen][Iluvatar] Add special_hermite_polynomial_h Iluvatar specialized operator (#373)
    Co-authored-by: xuejing.li <xjli@baai.ac.cn>
    (cherry picked from commit 88467639d34cc121929d26e526f3585c56959417)

--- src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
+++ src/flag_gems/runtime/backend/_iluvatar/ops/__init__.py
@@ -69,10 +69,7 @@ from .special_chebyshev_polynomial_w import (
     special_chebyshev_polynomial_w_out,
 )
 from .special_gammainc import special_gammainc
-from .special_hermite_polynomial_h import (
-    special_hermite_polynomial_h,
-    special_hermite_polynomial_h_tensor_tensor,
-)
+from .special_hermite_polynomial_h import special_hermite_polynomial_h
 from .special_modified_bessel_k1 import (
     special_modified_bessel_k1,
     special_modified_bessel_k1_out,
@@ -150,7 +147,6 @@ __all__ = [
     "special_chebyshev_polynomial_w_out",
     "special_gammainc",
     "special_hermite_polynomial_h",
-    "special_hermite_polynomial_h_tensor_tensor",
     "special_modified_bessel_k1",
     "special_modified_bessel_k1_out",
     "special_shifted_chebyshev_polynomial_w",
