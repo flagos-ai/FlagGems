6c0d4bffacaf91f1eaa12d836937201751ca36c3
bwbwzzz <96050438+bwbwzzz@users.noreply.github.com>
2026-09-11 16:47:34 +0800

    Add Hygon operator: addmv_ (#572)
    Co-authored-by: Dongxu-H <dxhan@baai.ac.cn>
    (cherry picked from commit 766efaeb47b915e5ba2df285286d9a370bdfea27)

--- src/flag_gems/runtime/backend/_hygon/ops/__init__.py
+++ src/flag_gems/runtime/backend/_hygon/ops/__init__.py
@@ -19,6 +19,7 @@ from ._scaled_dot_product_flash_attention import _scaled_dot_product_flash_atten
 from .adaptive_avg_pool2d_backward import adaptive_avg_pool2d_backward
 from .adaptive_max_pool2d_backward import adaptive_max_pool2d_backward
 from .adaptive_max_pool3d_backward import adaptive_max_pool3d_backward
+from .addmv_ import addmv_
 from .addr import addr
 from .amp_foreach_non_finite_check_and_unscale_ import (
     amp_foreach_non_finite_check_and_unscale_,
@@ -169,6 +170,7 @@ __all__ = [
     "adaptive_avg_pool2d_backward",
     "adaptive_max_pool2d_backward",
     "adaptive_max_pool3d_backward",
+    "addmv_",
     "addr",
     "amp_foreach_non_finite_check_and_unscale_",
     "any",
