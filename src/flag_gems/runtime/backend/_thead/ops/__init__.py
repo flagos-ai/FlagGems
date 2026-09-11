83957c837100b3edfed966db13e2f8326f0d437a
chx7514 <49188554+chx7514@users.noreply.github.com>
2026-09-11 11:19:41 +0800

    [KernelGen][Thead] Add special_gammaln T-Head specialized operator (#325)
    (cherry picked from commit 2a221b6e0754b08f8865c89a625610ab07d8ecb5)

--- src/flag_gems/runtime/backend/_thead/ops/__init__.py
+++ src/flag_gems/runtime/backend/_thead/ops/__init__.py
@@ -51,6 +51,7 @@ from .special_chebyshev_polynomial_w import (
 )
 from .special_erfinv import special_erfinv, special_erfinv_, special_erfinv_out
 from .special_gammainc import special_gammainc
+from .special_gammaln import special_gammaln
 from .special_hermite_polynomial_h import (
     special_hermite_polynomial_h,
     special_hermite_polynomial_h_tensor_tensor,
@@ -115,6 +116,7 @@ __all__ = [
     "special_erfinv_",
     "special_erfinv_out",
     "special_gammainc",
+    "special_gammaln",
     "special_hermite_polynomial_h",
     "special_hermite_polynomial_h_tensor_tensor",
     "special_multigammaln",
