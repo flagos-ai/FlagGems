04caee55d0be4945f2cf7f89b3a939ac15e1d153
KK <102746679+Yukun-Cui@users.noreply.github.com>
2026-09-14 11:00:52 +0800

    [KernelGen][MThreads] Add ormqr Moore Threads specialized operator (#443)
    (cherry picked from commit d8c4daa2698be6438b9f29d62294cb311cc6a63c)

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -98,6 +98,7 @@ from .normal import normal_
 from .one_hot import one_hot
 from .ones import ones
 from .ones_like import ones_like
+from .ormqr import ormqr
 from .pad import constant_pad_nd, pad
 from .permute_copy import permute_copy
 from .prod import prod, prod_dim
@@ -325,6 +326,7 @@ if get_device_capability(current_device())[0] >= 3:
     "lcm_",
     "lift_fresh",
     "addmv_",
+    "ormqr",
         ]
     )
 
