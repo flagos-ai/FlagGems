25cffa16d3b2949b9746e9bb739f3302db545d9b
小兔子爱吃大灰狼 <81555989+youseeyouonedayday@users.noreply.github.com>
2026-09-17 03:35:15 +0000

    [KernelGen][Nvidia] Add _dimV operator
    (cherry picked from commits 41ed65a8b6228614ea63da6f385e2e8d00d964fd, 9b7f1fcbef7222417203d2295104728e9e81f75c in flagos-ai/FlagGems-Experimental)

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -150,6 +150,7 @@ _FULL_CONFIG = (
     ("_cudnn_rnn_backward", cudnn_rnn_backward),
     ("_cummax_helper", _cummax_helper),
     ("_cummin_helper", _cummin_helper),
+    ("_dimV", _dimV, None, (SPARSE_DISPATCH_KEY,)),
     ("_dirichlet_grad", _dirichlet_grad, lambda: _HAS_MAP_ELEMENTWISE),
     ("_dyn_quant_matmul_4bit", _dyn_quant_matmul_4bit),
     ("_dyn_quant_pack_4bit_weight", _dyn_quant_pack_4bit_weight),
