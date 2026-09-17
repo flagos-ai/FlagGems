23c2d94f362b4d65e232c4278cb8fb9abd6e9fe4
小兔子爱吃大灰狼 <81555989+youseeyouonedayday@users.noreply.github.com>
2026-09-17 04:27:05 +0000

    [KernelGen][Nvidia] Add _has_same_storage_numel operator
    (cherry picked from commits 2bd855666d5f6364c4e95d61de5825ac30f78dab, 7f3e8e06becd615a4758ea8f53f9b198fcef66bd, 60d3c49cb129cb2bb3cb2e905d2324cdaee3feb7, c56acf71a208705847627849e42f1c98901437ed in flagos-ai/FlagGems-Experimental)

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -209,6 +209,7 @@ _FULL_CONFIG = (
         None,
         (AUTOGRAD_DISPATCH_KEY,),
     ),
+    ("_has_same_storage_numel", _has_same_storage_numel),
     ("_index_put_impl_", _index_put_impl_),
     (
         "_int_mm",
