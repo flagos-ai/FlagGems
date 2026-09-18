43473718825e248be9198fc881cc4d004d23a8d3
LoserCheems <losercheems@gmail.com>
2026-09-18 16:48:04 +0800

    [KernelGen][Nvidia] Add batch_norm_elemt operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -474,6 +474,7 @@ _FULL_CONFIG = (
     ("baddbmm", baddbmm),
     ("baddbmm.out", baddbmm_out),
     ("baddbmm_", baddbmm_),
+    ("batch_norm_elemt", batch_norm_elemt),
     ("batch_norm_gather_stats_with_counts", batch_norm_gather_stats_with_counts),
     ("bernoulli", bernoulli),
     ("bernoulli_.float", bernoulli_),
