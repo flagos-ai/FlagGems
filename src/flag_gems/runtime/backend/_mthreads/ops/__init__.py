4ac0810b089960de207199ee37e73e1a386eb1c6
103yiran <1039105206@qq.com>
2026-09-20 03:35:11 +0000

    fix(_mthreads): fix mish_

--- src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
+++ src/flag_gems/runtime/backend/_mthreads/ops/__init__.py
@@ -105,7 +105,8 @@ from .max import max, max_dim
 from .max_pool2d_with_indices_backward import max_pool2d_with_indices_backward
 from .median import median, median_dim, median_dim_values, median_out
 from .min import min, min_dim
-from .mish import mish, mish_
+from .mish import mish
+from .mish_ import mish_
 from .mode import mode
 from .mul import mul, mul_
 from .mvlgamma import mvlgamma
