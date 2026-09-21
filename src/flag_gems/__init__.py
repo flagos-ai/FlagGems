f9d451c4b17d98474bad1ed6c88897e43b9a7145
LoserCheems <losercheems@gmail.com>
2026-09-21 11:03:05 +0800

    [KernelGen][Nvidia] Add fractional_max_pool3d_backward operator with Triton kernel

--- src/flag_gems/__init__.py
+++ src/flag_gems/__init__.py
@@ -755,6 +755,7 @@ _FULL_CONFIG = (
     ("fractional_max_pool2d", fractional_max_pool2d),
     ("fractional_max_pool2d_backward", fractional_max_pool2d_backward),
     ("fractional_max_pool3d", fractional_max_pool3d),
+    ("fractional_max_pool3d_backward", fractional_max_pool3d_backward),
     ("frexp", frexp),
     ("full", full),
     ("full_like", full_like),
